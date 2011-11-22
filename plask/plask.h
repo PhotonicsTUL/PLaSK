68703ac8b1f0c230ff4900ad6e352ad0e52b5846
piotr <piotr@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2011-11-22 15:29:44 +0000

    commit missing file [r40]

---
+++ plask/plask.h
@@ -0,0 +1,18 @@
+#ifndef PLASK__PLASK_H
+#define PLASK__PLASK_H
+
+//allow for easy include all plask futures, also includes manual main page in doxygen format
+
+/** @mainpage libplask manual
+
+@section plask_about About
+Plask is...
+
+@section plask_tutorials Tutorials
+- @subpage modules "How to implement own module?"
+- @subpage providers "How to use and implement providers and recivers?"
+
+*/
+
+
+#endif // PLASK__PLASK_H
