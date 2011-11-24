ea69b8fcbbb6557bcda43cc8f3f13d6c81b1e6e8
maciek <maciek@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2011-11-24 22:49:34 +0000

    Added directories and testing framework for Python modules. [r48]

---
+++ tests/python/test_providers.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+
+import random
+
+# TODO: write proper test cases
+
+class SimpleProviders(unittest.TestCase):
+
+    def setUp(self):
+        '''test case initialization'''
+        self.seq = range(10)
+
+    def test_Shuffle(self):
+        # make sure the shuffled sequence does not lose any elements
+        random.shuffle(self.seq)
+        self.seq.sort()
+        self.assertEqual(self.seq, range(10))
+
+        # should raise an exception for an immutable sequence
+        self.assertRaises(TypeError, random.shuffle, (1,2,3))
+
+    def test_Choice(self):
+        element = random.choice(self.seq)
+        self.assertTrue(element in self.seq)
+
+    def test_Sample(self):
+        with self.assertRaises(ValueError):
+            random.sample(self.seq, 20)
+        for element in random.sample(self.seq, 5):
+            self.assertTrue(element in self.seq)
