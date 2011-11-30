26c23ada9b18c0c8f03e02f2bb26036d660f9496
piotr <piotr@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2011-11-30 09:48:13 +0000

    fixed compilation bugs [r68]

--- tests/plask/vectors.cpp
+++ tests/plask/vectors.cpp
@@ -6,15 +6,15 @@
 BOOST_AUTO_TEST_SUITE(vectors)
 
 BOOST_AUTO_TEST_CASE(Vector2d) {
-    plask::Vector2d<double> v_d(1.0, 2.0);
-    BOOST_CHECK(v_d.x == 1.0 && v_d.y == 2.0);
+    plask::Vec2<double> v_d(1.0, 2.0);
+    BOOST_CHECK(v_d.c0 == 1.0 && v_d.c1 == 2.0);
    
     //plask::Vector2d<float> v_f(1.0, 2.0);
 }
 
 BOOST_AUTO_TEST_CASE(Vector3d) {
-    plask::Vector3d<double> v_d(1.0, 2.0, 3.0);
-    BOOST_CHECK(v_d.x == 1.0 && v_d.y == 2.0 && v_d.z == 3.0);
+    plask::Vec3<double> v_d(1.0, 2.0, 3.0);
+    BOOST_CHECK(v_d.c0 == 1.0 && v_d.c1 == 2.0 && v_d.c2 == 3.0);
    
     //plask::Vector2d<float> v_f(1.0, 2.0);
 }
