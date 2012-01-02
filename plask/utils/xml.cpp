7dc54fd754005dd1ae552e6599b4c5226a13021c
piotr <piotr@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2012-01-02 10:42:37 +0000

    missing file [r138]

---
+++ plask/utils/xml.cpp
@@ -0,0 +1,35 @@
+#include "xml.h"
+
+#include "../exceptions.h"
+
+namespace plask {
+
+std::string requireAttr(XMLReader &source, const char* attr_name) {
+    const char* result = source.getAttributeValue(attr_name);
+    if (result == nullptr)
+        throw XMLNoAttrException(source.getNodeName(), attr_name);
+    return result;
+}
+
+void requireNext(XMLReader& reader) {
+    do {
+        if (!reader.read())
+            throw XMLUnexpectedEndException();
+    } while (reader.getNodeType() == irr::io::EXN_COMMENT); 
+}
+
+void requireTag(XMLReader& reader) {
+    requireNext(reader);
+    if (reader.getNodeType() != irr::io::EXN_ELEMENT)
+        throw XMLUnexpectedElementException("begin of tag");    
+}
+
+void requireTagEnd(XMLReader& reader) {
+    requireNext(reader);
+    if (reader.getNodeType() != irr::io::EXN_ELEMENT_END)
+        throw XMLUnexpectedElementException("end of tag");
+}
+
+
+
+} // namespace plask
