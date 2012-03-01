96023cbdb45352db31a3bee828cc9ff1a1ff27ca
piotr <piotr@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2012-03-01 13:29:29 +0000

    [r338]

--- plask/material/info.cpp
+++ plask/material/info.cpp
@@ -1,2 +1,10 @@
 #include "info.h"
 
+namespace plask {
+
+MaterialInfoDB& MaterialInfoDB::getDefault() {
+    static MaterialInfoDB defaultInfoDB;
+    return defaultInfoDB;
+}
+
+}
--- plask/material/info.h
+++ plask/material/info.h
@@ -7,9 +7,98 @@ This file includes classes which stores meta-informations about materials.
 
 //TODO impl.
 
+#include <string>
+#include <map>
+#include <vector>
+#include <utility>
+
 namespace plask {
 
 class MaterialInfo {
+
+    enum PROPERTY_NAME {
+        kind,
+        lattC,
+        Eg,
+        CBO,
+        VBO,
+        Dso,
+        Mso,
+        Me,
+        Me_v,
+        Me_l,
+        Mhh,
+        Mhh_v,
+        Mhh_l,
+        Mlh,
+        Mlh_v,
+        Mlh_l,
+        Mh,
+        Mh_v,
+        Mh_l,
+        eps,
+        chi,
+        Nc,
+        Ni,
+        Nf,
+        EactD,
+        EactA,      ///<acceptor ionisation energy
+        mob,        ///<mobility
+        cond,       ///<electrical conductivity
+        cond_v,     ///<electrical conductivity in vertical direction
+        cond_l,     ///<electrical conductivity in lateral direction
+        res,        ///<electrical resistivity
+        res_v,      ///<electrical resistivity in vertical direction
+        res_l,      ///<electrical resistivity in lateral direction
+        A,          ///<monomolecular recombination coefficient
+        B,          ///<radiative recombination coefficient
+        C,          ///<Auger recombination coefficient
+        D,          ///<ambipolar diffusion coefficient
+        condT,      ///<thermal conductivity
+        condT_v,    ///<thermal conductivity in vertical direction
+        condT_l,    ///<thermal conductivity in lateral direction
+        dens,       ///<density
+        specHeat,   ///<specific heat at constant pressure
+        nr,         ///<refractive index
+        absp,       ///<absorption coefficient alpha
+        Nr          ///<refractive index
+    };
+
+    ///Names of arguments for which range we need give
+    enum ARGUMENT_NAME {
+        T,          ///<temperature [K]
+        thickness,  ///<thickness [m]
+        wl          ///<Wavelength [nm]
+    };
+
+    ///Name of parent class
+    std::string parent;
+
+    struct PropertyInfo {
+
+        typedef std::pair<double, double> ArgumentRange;
+
+        std::map<ARGUMENT_NAME, ArgumentRange> argumentRange;
+
+        std::string source;
+
+        std::string comment;
+    };
+
+    std::map<PROPERTY_NAME, PropertyInfo> propertyInfo;
+
+};
+
+class MaterialInfoDB {
+
+
+
+public:
+
+    static MaterialInfoDB& getDefault();
+
+    ///Material name -> material information
+    std::map<std::string, MaterialInfo> materialInfo;
 
 };
 
