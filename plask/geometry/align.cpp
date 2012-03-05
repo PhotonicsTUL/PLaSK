76dbdcbab478b64172df47441e26326f8cbb060f
piotr <piotr@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2012-03-05 10:08:45 +0000

    added missing returns [r345]

--- plask/geometry/align.cpp
+++ plask/geometry/align.cpp
@@ -10,17 +10,17 @@ namespace details {
 
 Aligner2d<DIRECTION_TRAN>* transAlignerFromString(std::string str) {
     boost::algorithm::to_lower(str);
-    if (str == "left" || str == "l") new Left();
-    if (str == "right" || str == "r") new Right();
-    if (str == "center" || str == "c") new Center();
+    if (str == "left" || str == "l") return new Left();
+    if (str == "right" || str == "r") return new Right();
+    if (str == "center" || str == "c" || str == "m" || str == "middle") return new Center();
     return new Tran(boost::lexical_cast<double>(str));
 }
 
 Aligner2d<DIRECTION_LON>* lonAlignerFromString(std::string str) {
     boost::algorithm::to_lower(str);
-    if (str == "front" || str == "f") new Front();
-    if (str == "back" || str == "b") new Back();
-    if (str == "center" || str == "c" || str == "LonCenter") new LonCenter();
+    if (str == "front" || str == "f") return new Front();
+    if (str == "back" || str == "b") return new Back();
+    if (str == "center" || str == "c" || str == "LonCenter" || str == "m" || str == "middle") return new LonCenter();
     return new Lon(boost::lexical_cast<double>(str));
 }
 
