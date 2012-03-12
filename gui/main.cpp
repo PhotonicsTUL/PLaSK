3f66a21864505dd99ada532b823698aa09bf85a7
piotr <piotr@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2012-03-12 10:25:49 +0000

    - gui program is one of qt examples compiled with cmake - fixed one of problem with cmake qt macros by remove "CXX" from project(...) [r355]

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,34 +1,52 @@
-#include <plask/geometry/element.h>
-#include <plask/geometry/stack.h>
-#include <chrono>
-#include <typeindex>
-#include <iostream>
+/****************************************************************************
+**
+** Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (qt-info@nokia.com)
+**
+** This file is part of the examples of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:BSD$
+** You may use this file under the terms of the BSD license as follows:
+**
+** "Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are
+** met:
+**   * Redistributions of source code must retain the above copyright
+**     notice, this list of conditions and the following disclaimer.
+**   * Redistributions in binary form must reproduce the above copyright
+**     notice, this list of conditions and the following disclaimer in
+**     the documentation and/or other materials provided with the
+**     distribution.
+**   * Neither the name of Nokia Corporation and its Subsidiary(-ies) nor
+**     the names of its contributors may be used to endorse or promote
+**     products derived from this software without specific prior written
+**     permission.
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+** $QT_END_LICENSE$
+**
+****************************************************************************/
 
-int main() {
-    
-    plask::shared_ptr<plask::GeometryElement> g(new plask::MultiStackContainer<2>());
-    plask::shared_ptr<plask::GeometryElement> g2(new plask::StackContainer<2>());
-    
-    auto t1 = std::chrono::high_resolution_clock::now();
-    
-    for (int i = 0; i < 200000; ++i) {
-        if (std::type_index(typeid(*g)) !=
-                std::type_index(typeid(*g2))) ;
-        std::type_index(typeid(*g)) ==
-        std::type_index(typeid(*g));
-        std::type_index(typeid(*g));
-        
-        /*plask::dynamic_pointer_cast<plask::MultiStackContainer<2>>(g);
-        plask::dynamic_pointer_cast<plask::MultiStackContainer<2>>(g);
-        plask::dynamic_pointer_cast<plask::MultiStackContainer<2>>(g);
-        
-        plask::dynamic_pointer_cast<plask::MultiStackContainer<2>>(g);
-        plask::dynamic_pointer_cast<plask::MultiStackContainer<2>>(g);*/
-    }
-    
-    auto t2 = std::chrono::high_resolution_clock::now();
-    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(t2-t1).count()
-              << " milliseconds / 1 000 000\n";
-    
-    return 0;
+#include <QApplication>
+
+#include "mainwindow.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    Q_INIT_RESOURCE(plaskgui);
+    MainWindow mainWin;
+    mainWin.show();
+    return app.exec();
 }
