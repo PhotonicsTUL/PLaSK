c8df9670940b00cd1a0056ea85e97d9903351ee6
piotr <piotr@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2012-03-20 15:20:47 +0000

    receiver.setConst implementation [r384]

--- tests/plask/providers.cpp
+++ tests/plask/providers.cpp
@@ -25,6 +25,9 @@ BOOST_AUTO_TEST_CASE(single_value) {
     receiver.setProvider(0);
     BOOST_CHECK(receiver.changed);
     BOOST_CHECK_THROW(receiver(), plask::NoProvider);
+
+    receiver.setConst(2.0);
+    BOOST_CHECK_EQUAL(receiver(), 2.0);
 }
 
 BOOST_AUTO_TEST_CASE(delegate_to_member) {
