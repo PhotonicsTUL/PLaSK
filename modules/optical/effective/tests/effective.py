4469e68bc7b644bd6a27225638affd78cd3ed927
maciek <maciek@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2012-05-17 14:33:19 +0000

    Added some tests to effective-index module. [r545]

---
+++ modules/optical/effective/tests/effective.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+
+from numpy import *
+
+from plask import *
+from plask import geometry
+from plask.optical.effective import EffectiveIndex2D
+
+
+
+class EffectiveIndex2D_Test(unittest.TestCase):
+
+    def setUp(self):
+        self.module = EffectiveIndex2D()
+        rect = geometry.Rectangle(2,1, "GaN")
+        stack = geometry.Stack2D()
+        for i in range(4): stack.append(rect)
+        hint = stack.append(rect)
+        for i in range(5): stack.append(rect)
+        space = Space2DCartesian(stack, left="mirror")
+        self.module.geometry = space
+        self.module.setInterfaceOn(hint)
+
+    def testInterface(self):
+        self.assertEqual( self.module.interface, 5 )
+        self.assertEqual( list(self.module.mesh.axis1), range(11) )
+
+    def testSymmetry(self):
+        self.assertIsNone( self.module.symmetry )
+        self.module.symmetry = "-"
+        self.assertEqual( self.module.symmetry, "negative" )
