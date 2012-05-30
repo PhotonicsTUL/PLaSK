d3c7b15eb36638d9748ed122d33d4312632103ed
piotr <piotr@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2012-05-30 08:48:36 +0000

    merga two memory.h files [r579]

--- plask/memory.h
+++ plask/memory.h
@@ -2,7 +2,7 @@
 #define PLASK__MEMORY_H
 
 /** @file
-This file includes utils connected with memory managment.
+This file includes utils connected with memory managment, it includes utils to operate on memory, pointers, smart pointers, etc.
 It put smart pointers (boost or std ones - dependly of plask build configuration) in plask namespace.
 */
 
@@ -113,6 +113,39 @@ struct Holder {
     ~Holder() { delete holded; }
     
 };
+
+/**
+ * Copy @a ptr data if is not the only shared_ptr instance managing the current object, i.e. whether ptr.unique() is @c false (ptr.use_count() != 1).
+ * @param ptr shared pointer
+ * @return @a ptr if ptr.unique() is @c true (ptr.use_count() == 1) or shared_ptr with copy of object managing by @a ptr if ptr.unique() is @c false
+ * @tparam T type of object managing by @a ptr, must have copy constructor
+ */
+template <typename T>
+inline shared_ptr<T> getUnique(const shared_ptr<T>& ptr) {
+    return ptr.unique() ? ptr : new T(*ptr);
+}
+
+/**
+ * Copy @a ptr data if is not the only shared_ptr instance managing the current object, i.e. whether ptr.unique() is @c false (ptr.use_count() != 1).
+ * @param ptr shared pointer
+ * @return @a ptr if ptr.unique() is @c true (ptr.use_count() == 1) or shared_ptr with copy of object managing by @a ptr if ptr.unique() is @c false
+ * @tparam T type of object managing by @a ptr, must have copy constructor
+ */
+template <typename T>
+inline shared_ptr<T> getUnique(const shared_ptr<const T>& ptr) {
+    return ptr.unique() ? ptr : new T(*ptr);
+}
+
+/**
+ * Copy @a ptr data if is not the only shared_ptr instance managing the current object, i.e. whether ptr.unique() is @c false (ptr.use_count() != 1).
+ * @param ptr shared pointer which will be changed to shared_ptr with copy of object managing by @a ptr if ptr.unique() is @c false
+ * @tparam T type of object managing by @a ptr, must have copy constructor
+ */
+template <typename T>
+inline void makeUnique(shared_ptr<T>& ptr) {
+    ptr = getUnique(ptr);
+}
+
 }   // namespace plask
 
 #endif // PLASK__MEMORY_H
