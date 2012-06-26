83416a625bd4da5919c65e5a872674171248eac2
maciek <maciek@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2012-06-26 14:42:37 +0000

    Added RegularMesh2d and RegularMesh3d (no Python interface yet through). [r622]

--- plask/mesh/regular.h
+++ plask/mesh/regular.h
@@ -13,13 +13,33 @@ This file includes rectilinear meshes for 1d, 2d, and 3d spaces.
 namespace plask {
 
 /// Two-dimensional rectilinear mesh type
-typename RectangularMesh2d<RegularMesh1d> RegularMesh2d;
+typedef RectangularMesh2d<RegularMesh1d> RegularMesh2d;
 
 /// Three-dimensional rectilinear mesh type
-typename RectangularMesh3d<RegularMesh1d> RegularMesh3d;
-
-
+typedef RectangularMesh3d<RegularMesh1d> RegularMesh3d;
 
 } // namespace plask
+
+
+namespace std { // use fast iterator if we know mesh type at compile time:
+
+    inline auto begin(const plask::RegularMesh2d& m) -> decltype(m.begin_fast()) {
+        return m.begin_fast();
+    }
+
+    inline auto end(const plask::RegularMesh2d& m) -> decltype(m.end_fast()) {
+        return m.end_fast();
+    }
+
+    inline auto begin(const plask::RegularMesh3d& m) -> decltype(m.begin_fast()) {
+        return m.begin_fast();
+    }
+
+    inline auto end(const plask::RegularMesh3d& m) -> decltype(m.end_fast()) {
+        return m.end_fast();
+    }
+
+} // namespace std
+
 
 #endif // PLASK__REGULAR_H
