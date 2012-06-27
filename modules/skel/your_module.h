e2d76b643a1f4ad7c5a075ffefa4a4913240f1ba
piotr <piotr@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2012-06-27 12:50:13 +0000

    fixed skel to don't cause makefile generation problems [r628]

--- modules/skel/your_module.h
+++ modules/skel/your_module.h
@@ -41,18 +41,7 @@ struct YourModule: public ModuleWithMesh<ForExample_Space2DCartesian, ForExample
      * Add short description of every method in a comment like this.
      * \param parameter some value to be provided by the user for computationsś
      **/
-    void compute(double parameter) {
-        // The code of this method probably should be in cpp file...
-        // But below we show some key elements
-        initCalculation(); // This must be called before any calculation!
-        writelog(LOG_INFO, "Begining calculation of something");
-        auto temperature = inTemperature(*mesh); // Obtain temperature from some other module
-        // [...] Do your computations here
-        outSingleValue = new_computed_value;
-        writelog(LOG_RESULT, "Found new value of something = $1$", new_computed_value);
-        outSingleValue.fireChanged(); // Inform other modules that you have computed a new value
-        outSomeField.fireChanged();
-    }
+    void compute(double parameter);
 
   protected:
 
