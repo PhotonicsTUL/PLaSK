ad3eae234f77ca59cb38f483e68ed02c3b8f5dff
maciek <maciek@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2012-09-03 12:41:31 +0000

    plask.pylab functions return more sensible things. [r852]

--- python/plask/pylab.py
+++ python/plask/pylab.py
@@ -25,8 +25,7 @@ def plotField(field, axes=None, **kwargs):
     '''Plot scalar real fields using pylab.imshow.'''
     #TODO documentation
     if axes is None: axes = matplotlib.pylab.gca()
-    axes.imshow(field.array, origin='lower', extent=[field.mesh.axis0[0], field.mesh.axis0[-1], field.mesh.axis1[0], field.mesh.axis1[-1]], **kwargs)
-    return axes
+    return axes.imshow(field.array, origin='lower', extent=[field.mesh.axis0[0], field.mesh.axis0[-1], field.mesh.axis1[0], field.mesh.axis1[-1]], **kwargs)
 
 
 def plotGeometry2D(geometry, axes=None, color='k', width=1.0, set_limits=False, zorder=3, mirror=False):
@@ -35,24 +34,29 @@ def plotGeometry2D(geometry, axes=None, color='k', width=1.0, set_limits=False, 
 
     import matplotlib.patches
     if axes is None: axes = matplotlib.pylab.gca()
+
+    patches = []
     for leaf,box in zip(geometry.getLeafsAsTranslations(), geometry.getLeafsBBoxes()):
         #TODO other shapes than rectangles
         def add_path(bottom):
             lefts = [box.lower[0]]
             if mirror and (geometry.borders['left'] == 'mirror' or geometry.borders['right']): lefts.append(-box.upper[0])
             for left in lefts:
-                axes.add_patch(matplotlib.patches.Rectangle([left, bottom],
+                patches.append(matplotlib.patches.Rectangle([left, bottom],
                                                             box.upper[0]-box.lower[0], box.upper[1]-box.lower[1],
                                                             ec=color, lw=width, fill=False, zorder=zorder))
         add_path(box.lower[1])
         if mirror and (geometry.borders['top'] == 'bottom' or geometry.borders['right']):
             add_path(-box.upper[1])
 
+    for patch in patches:
+        axes.add_patch(patch)
+
     if set_limits:
         box = geometry.bbox
         axes.set_xlim(box.lower[0], box.upper[0])
         axes.set_ylim(box.lower[1], box.upper[1])
-    return axes
+    return patches
 
 
 def plotMesh2D(mesh, axes=None, color='0.5', width=1.0, set_limits=False, zorder=2):
@@ -62,18 +66,21 @@ def plotMesh2D(mesh, axes=None, color='0.5', width=1.0, set_limits=False, zorder
     import matplotlib.lines
     if axes is None: axes = matplotlib.pylab.gca()
 
+    lines = []
     if type(mesh) in [plask.mesh.Regular2D, plask.mesh.Rectilinear2D]:
         y_min = mesh.axis1[0]; y_max = mesh.axis1[-1]
         for x in mesh.axis0:
-            axes.add_line(matplotlib.lines.Line2D([x,x], [y_min,y_max], color=color, lw=width, zorder=zorder))
+            lines.append(matplotlib.lines.Line2D([x,x], [y_min,y_max], color=color, lw=width, zorder=zorder))
         x_min = mesh.axis0[0]; x_max = mesh.axis0[-1]
         for y in mesh.axis1:
-            axes.add_line(matplotlib.lines.Line2D([x_min,x_max], [y,y], color=color, lw=width, zorder=zorder))
-        if set_limits:
-            axes.set_xlim(x_min, x_max)
-            axes.set_ylim(y_min, y_max)
+            lines.append(matplotlib.lines.Line2D([x_min,x_max], [y,y], color=color, lw=width, zorder=zorder))
 
+    for line in lines:
+        axes.add_line(line)
 
+    if set_limits:
+        axes.set_xlim(x_min, x_max)
+        axes.set_ylim(y_min, y_max)
 
-    return axes
+    return lines
 
