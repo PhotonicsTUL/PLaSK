723703d0d4e89fbb6a093032a8b8317eccf8de6c
maciek <maciek@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2012-10-24 15:06:10 +0000

    Writing dan2xpl. [r1098]

--- tools/dan2xpl.py
+++ tools/dan2xpl.py
@@ -1,4 +1,4 @@
-#!/bin/python
+#!/usr/bin/python
 '''
     This tool converts RPSMES .dan file to PLaSK .xpl file.
 
@@ -9,14 +9,14 @@
     If the conversion is successful, the script writes
 '''
 import sys
-
+import traceback
 
 
 try:
     ifile = open(sys.argv[1])
 except IndexError:
     sys.stderr.write("Usage: "+sys.argv[0]+" input_file_temp.dan\n")
-    sys.exit(1)
+    sys.exit(3)
 except IOError:
     sys.stderr.write("Cannot read file "+sys.argv[1]+"\n")
     sys.exit(2)
@@ -25,27 +25,77 @@ except IOError:
 # Set-up generator, which skips empty lines, strips the '\n' character, and splits line by tabs
 def Input(ifile):
     for line in ifile:
-        if line.strip(): yield line[:-1].split('\t')
+        print line[:-1]
+        if line.strip(): yield line[:-1].split()
 input = Input(ifile)
 
-try: # Reading the file
+# Header
+name = input.next()[0]                              # structure name (will be used for output file)
+matdb = input.next()[0]                             # materials database spec (All by default)
+line = input.next()                                 # symmetry (0: Cartesian2D, 1: cylindrical) type and height (not used)
+symmetry = ['cartesian2d', 'cylindrical'][int(line[0])]
+input.next()                                        # horizontal something (not used)
+line = input.next()                                 # number of defined regions and scale
+nregions = int(line[0])
+try: scale = float(line[1])
+except ValueError: scale = float(line[2])
 
-    # Header
-    name = input.next()[0]                              # structure name (will be used for output file)
-    matdb = input.next([0]                              # materials database spec (All by default)
-    line = input.next()                                 # symmetry (0: Cartesian2D, 1: cylindrical) type and height (not used)
-    symmetry = ['cartesian2d', 'cylindrical'][int(line[0])]
-    input.next()                                        # horizontal something (not used)
-    line = input.next()                                 # number of defined regions and scale
-    nregions, scale = int(line[0]), float(line[1])
+regions = []
+materials = {}
 
-    # Read each region
-    for i in range(nregions):
+# Read each region
+for i in range(nregions):
+    r = {}
+
+    # number, position, material
+    line = input.next()
+    n = int(line[0])
+    if n == 0:
+        repeats = int(line[1])
+        shift = float(line[2])
+        si = {'pionowo': 1, 'poziomo': 0}[line[3].lower()]
         line = input.next()
-        n = int(line[0]); if n != i: throw ValueError("wrong region number")
-        x1, y1, x2, y2 = map(float, line[1:5])
-        material = line[6]
+    else:
+        repeats = 1
+        shift = 0.
+        si = 0
+    x1, y1, x2, y2 = map(float, line[1:5])
+    r['lower'], r['upper'] = [x1, y1], [x2, y2]
+    mat = line[5]
+    r['mat'] = mat
 
-except Exception as err:
-    sys.stderr.write("Error reading file "+sys.argv[1]+":\n  " + type(err)+": "+err.message+"\n")
-    sys.exit(3)
+    # TODO: handle situations where there is material composition instead of sigma and kappa
+
+    # conductivity
+    line = input.next()
+    r['sigma_x'], r['sigma_y'] = float(line[0]), float(line[1])
+    r['sigma_t'] = line[2]
+
+    # doping
+    line = input.next()
+    r['doping'], r['dopant'] = float(line[0]), line[1]
+
+    # heat conductivity
+    line = input.next()
+    r['kappa_x'], r['kappa_y'] = float(line[0]), float(line[1])
+    r['kappa_t'] = line[2]
+
+    # heat sources
+    line = input.next()
+    ht = int(line[0])
+    r['heat'] = 0
+    r['role'] = None
+    if ht == -200: r['role'] == 'active'
+    elif ht == 0: r['role'] == 'insulator'
+    elif ht == -1: r['heat'] = float(line[1])
+    elif ht != -100: raise ValueError("wrong heat source type")
+
+    # save to the list (TODO: make it more clever, using heuristic algorithms to construct stacks and shelves)
+    resions.append(r)
+    for j in range(repeats-1):
+        r = r.copy()
+        r['lower'][si] += shift
+        r['upper'][si] += shift
+        resions.append(r)
+
+# Read boundary conditions
