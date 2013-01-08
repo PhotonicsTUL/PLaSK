b38055dc203079fb991767e910d9c0569db904fa
piotr <piotr@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2013-01-08 11:41:31 +0000

    some work on align container [r1244]

--- plask/geometry/align_container.h
+++ plask/geometry/align_container.h
@@ -5,6 +5,7 @@
 This file includes containers of geometries objects which align all children in one direction and allow to explicitly choose coordinates in rest directions.
 */
 
+#include "container.h"
 #include "align.h"
 #include "../utils/metaprog.h"
 
@@ -16,7 +17,7 @@ namespace plask {
  */
 //TODO implementation
 template <int dim, typename Primitive<dim>::Direction alignDirection>
-class AlignContainer {
+class AlignContainer: public GeometryObjectContainer<dim> {
 
     typedef typename chooseType<dim-2, align::OneDirectionAligner<direction2Dto3D(alignDirection)>, align::OneDirectionAligner<alignDirection> >::type Aligner;
 
@@ -24,6 +25,26 @@ class AlignContainer {
      * Aligner which is use to align object in alignDirection.
      */
     std::unique_ptr<Aligner> aligner;
+    
+public:
+    
+    AlignContainer(const Aligner& aligner)
+        : aligner(aligner.cloneUnique())
+    {}
+    
+    /// Called by child.change signal, update heights call this change
+    void onChildChanged(const GeometryObject::Event& evt) {
+        if (evt.isResize()) aligner->align(evt.source());
+        GeometryObjectContainer<dim>::onChildChanged(evt);
+    }
+    
+    const Aligner& getAligner() const {
+        return *aligner;
+    }
+    
+    void setAligner(const Aligner& new_aligner) const {
+        aligner = new_aligner.cloneUnique();
+    }
     
 };
 
