5293a414945da84ce45ef18afa73b9d534a2b82b
piotr <piotr@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2013-04-17 19:41:02 +0000

    minor [r1446]

--- tests/plask/filters.cpp
+++ tests/plask/filters.cpp
@@ -7,8 +7,8 @@ BOOST_AUTO_TEST_SUITE(filters) // MUST be the same as the file name
         struct DoubleField: public plask::FieldProperty<double> {};
 
         plask::Filter<DoubleField, plask::Geometry2DCartesian> filter2D(plask::make_shared<plask::Geometry2DCartesian>());
-        //filter2D.setDefault(1.0);
-
+        filter2D.setDefault(1.0);
+        //BOOST_CHECK_EQUAL(filter2D.out.);
 
         //BOOST_CHECK(false);
     }
