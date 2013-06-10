7486ca4d94d9a8dd3146a72c6a1bb41a684e274b
piotr <piotr@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2013-06-10 15:08:15 +0000

    test for filters with vector fields [r1519]

--- tests/plask/filters.cpp
+++ tests/plask/filters.cpp
@@ -23,14 +23,14 @@ struct TestEnvGeom2D {
 struct TestEnvGeom3D {
 
     plask::shared_ptr<plask::Block<3>> block111;
+    plask::shared_ptr<plask::Block<2>> block11_in_extr;
     plask::shared_ptr<plask::Extrusion> extr211;
     plask::shared_ptr<plask::TranslationContainer<3>> container;
 
     TestEnvGeom3D() {
         block111 = plask::make_shared<plask::Block<3>>(plask::vec(1.0, 1.0, 1.0), plask::make_shared<DumbMaterial>());
-        extr211 = plask::make_shared<plask::Extrusion>(
-                plask::make_shared<plask::Block<2>>(plask::vec(1.0, 1.0), plask::make_shared<DumbMaterial>()),
-                2.0);
+        block11_in_extr = plask::make_shared<plask::Block<2>>(plask::vec(1.0, 1.0), plask::make_shared<DumbMaterial>());
+        extr211 = plask::make_shared<plask::Extrusion>(block11_in_extr, 2.0);
         container = plask::make_shared<plask::TranslationContainer<3>>();
         container->add(block111, plask::vec(1.0, 1.0, 1.0));    // to [2, 2, 2]
         container->add(block111, plask::vec(2.0, 2.0, 2.0));    // to [3, 3, 3]
@@ -78,6 +78,14 @@ BOOST_AUTO_TEST_SUITE(filters) // MUST be the same as the file name
         TestEnvGeom3D e;
 
         plask::Filter<VectorField, plask::Geometry3D> filter3D(plask::make_shared<plask::Geometry3D>(e.container));
+        filter3D.setDefault(plask::vec(1.0, 1.0, 1.0));
+        filter3D.appendInner(e.block111) = plask::vec(2.0, 2.0, 2.0);
+        filter3D.appendInner2D(e.extr211) = plask::vec(3.0, 3.0);
+
+        BOOST_CHECK_EQUAL(filter3D.out(plask::toMesh(plask::vec(0.5, 0.5, 0.5)), plask::DEFAULT_INTERPOLATION), plask::DataVector<plask::Vec<3>>{ plask::vec(1.0, 1.0, 1.0) });
+        BOOST_CHECK_EQUAL(filter3D.out(plask::toMesh(plask::vec(1.5, 1.5, 1.5)), plask::DEFAULT_INTERPOLATION), plask::DataVector<plask::Vec<3>>{ plask::vec(2.0, 2.0, 2.0) });
+        BOOST_CHECK_EQUAL(filter3D.out(plask::toMesh(plask::vec(2.5, 2.5, 2.5)), plask::DEFAULT_INTERPOLATION), plask::DataVector<plask::Vec<3>>{ plask::vec(2.0, 2.0, 2.0) });
+        BOOST_CHECK_EQUAL(filter3D.out(plask::toMesh(plask::vec(4.5, 3.5, 3.5)), plask::DEFAULT_INTERPOLATION), plask::DataVector<plask::Vec<3>>{ plask::vec(0.0, 3.0, 3.0) });
     }
 
 BOOST_AUTO_TEST_SUITE_END()
