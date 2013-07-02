025d3c48eccf0944d273a0541b13b67085c52964
piotr <piotr@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2013-07-02 15:00:25 +0000

    tools to build VC9 compatibile MXE env. [r1533]

--- mingw/manifest_mingw32.sh
+++ mingw/manifest_mingw32.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 python create_manifest.py
 i686-pc-mingw32-windres --input msvcr.rc --output msvcrc.o
+
+i686-pc-mingw32-gcc -dumpspecs | sed -e '/\*cpp:/ {n; s/^/-D__MSVCRT_VERSION__=0x0900 /}' -e 's/-lmoldname/-lmoldname90/' -e 's/-lmsvcrt/-lmsvcr90/' > specs
