85a06f493e3acf0bad494f2140f23e1267e99f2c
maciek <maciek@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2013-09-25 11:42:34 +0000

    Some documentation improvements. [r1691]

--- toolset/mingw/run_cmake.sh
+++ toolset/mingw/run_cmake.sh
@@ -14,14 +14,15 @@ esac
 rm CMakeCache.txt 2>/dev/null
 
 cmake .. -G Ninja \
-	 -DCMAKE_TOOLCHAIN_FILE=$MINGW_ROOT/usr/$arch-pc-$mingw/share/cmake/mxe-conf.cmake \
-	 -DNUMPY_INCLUDE_DIRS=$MINGW_ROOT/$arch/python27/Lib/site-packages/numpy/core/include \
-	 -DBUILD_TESTING=OFF \
-	 -DCMAKE_INSTALL_PREFIX=$MINGW_ROOT/dist \
-	 -DRUN_TESTS=OFF \
-	 -DUSE_OMP=OFF \
-	 -DGSL_CONFIG_EXECUTABLE=$MINGW_ROOT/usr/$arch-pc-$mingw/bin/gsl-config \
-	 -DDLLS_DIR=$MINGW_ROOT/$arch/dlls \
-         -DUSE_FFTW=OFF \
-	 -DCMAKE_BUILD_TYPE=Release	
-#	 -DCMAKE_BUILD_TYPE=Debug	
+    -DCMAKE_TOOLCHAIN_FILE=$MINGW_ROOT/usr/$arch-pc-$mingw/share/cmake/mxe-conf.cmake \
+    -DNUMPY_INCLUDE_DIRS=$MINGW_ROOT/$arch/python27/Lib/site-packages/numpy/core/include \
+    -DBUILD_TESTING=OFF \
+    -DCMAKE_INSTALL_PREFIX=$MINGW_ROOT/dist \
+    -DRUN_TESTS=OFF \
+    -DUSE_OMP=OFF \
+    -DGSL_CONFIG_EXECUTABLE=$MINGW_ROOT/usr/$arch-pc-$mingw/bin/gsl-config \
+    -DDLLS_DIR=$MINGW_ROOT/$arch/dlls \
+    -DUSE_FFTW=OFF \
+    -DBUILD_MANUAL=ON \
+    -DCMAKE_BUILD_TYPE=Release
+#     -DCMAKE_BUILD_TYPE=Debug
