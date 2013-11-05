0dc23a778243ab8ec43accac80ef5955c3b6c1b5
piotr <piotr@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2013-11-05 12:44:34 +0000

    minnor [r1799]

--- plask/manager.cpp
+++ plask/manager.cpp
@@ -516,7 +516,7 @@ std::string Manager::validatePositions() const {
     validatePositions(
     [&] (const Geometry* g1, const Geometry* g2, std::vector<const GeometryObject*>&& objs, const std::map<const GeometryObject*, const char*>& names) {
         if (first)
-            result += "Some objects, probably by mistake, have different positions in geometries (geometry 1 - geometry 2: name of objects...):";
+            result += "Some objects, possibly by mistake, have different positions in geometries (geometry 1 - geometry 2: name of objects...):";
         result += "\n ";
         result += geomName(*this, g1, names);
         result += " - ";
