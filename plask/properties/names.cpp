312744bb45d58506042b8aa5ef1d5d610064854f
maciek <maciek@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2014-01-06 10:19:28 +0000

    Added the file with missing constants definitions. [r1893]

---
+++ plask/properties/names.cpp
@@ -0,0 +1,32 @@
+#include "thermal.h"
+#include "electrical.h"
+#include "gain.h"
+#include "optical.h"
+
+namespace plask {
+
+constexpr const char* Temperature::NAME;                    constexpr const char* Temperature::UNIT;
+constexpr const char* HeatFlux::NAME;                       constexpr const char* HeatFlux::UNIT;
+constexpr const char* Heat::NAME;                           constexpr const char* Heat::UNIT;
+constexpr const char* ThermalConductivity::NAME;            constexpr const char* ThermalConductivity::UNIT;
+
+constexpr const char* Potential::NAME;                      constexpr const char* Potential::UNIT;
+constexpr const char* CurrentDensity::NAME;                 constexpr const char* CurrentDensity::UNIT;
+constexpr const char* CarriersConcentration::NAME;          constexpr const char* CarriersConcentration::UNIT;
+constexpr const char* ElectronsConcentration::NAME;         constexpr const char* ElectronsConcentration::UNIT;
+constexpr const char* HolesConcentration::NAME;             constexpr const char* HolesConcentration::UNIT;
+constexpr const char* ElectricalConductivity::NAME;         constexpr const char* ElectricalConductivity::UNIT;
+
+constexpr const char* Gain::NAME;                           constexpr const char* Gain::UNIT;
+constexpr const char* GainOverCarriersConcentration::NAME;  constexpr const char* GainOverCarriersConcentration::UNIT;
+
+constexpr const char* RefractiveIndex::NAME;                constexpr const char* RefractiveIndex::UNIT;
+constexpr const char* LightIntensity::NAME;                 constexpr const char* LightIntensity::UNIT;
+constexpr const char* OpticalElectricField::NAME;           constexpr const char* OpticalElectricField::UNIT;
+constexpr const char* OpticalMagneticField::NAME;           constexpr const char* OpticalMagneticField::UNIT;
+constexpr const char* Wavelength::NAME;                     constexpr const char* Wavelength::UNIT;
+constexpr const char* ModalLoss::NAME;                      constexpr const char* ModalLoss::UNIT;
+constexpr const char* PropagationConstant::NAME;            constexpr const char* PropagationConstant::UNIT;
+constexpr const char* EffectiveIndex::NAME;                 constexpr const char* EffectiveIndex::UNIT;
+
+}
