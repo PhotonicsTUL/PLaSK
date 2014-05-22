733bd44021ba12c53039239232e1f215c8868833
piotr <piotr@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2014-05-22 15:03:30 +0000

    BFD lib can also be used (if dw not found) to print backtrace [r2239]

---
+++ cmake/FindBFD.cmake
@@ -0,0 +1,13 @@
+# module oryginally from: https://github.com/Grive/grive/blob/master/cmake/Modules/FindBFD.cmake
+
+find_library( BFD_LIBRARY	NAMES bfd	PATH /usr/lib /usr/lib64 )
+
+if(BFD_LIBRARY)
+    set( BFD_FOUND TRUE )
+endif(BFD_LIBRARY)
+
+if (BFD_FOUND)
+    message(STATUS "Found libbfd: ${BFD_LIBRARY}")
+else (BFD_FOUND)
+    message(STATUS "Could NOT find libbfd")
+endif(BFD_FOUND)
