cb3eb3d7572b41205655701f93fd079ef5b6e644
piotr <piotr@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2014-06-12 10:32:32 +0000

    Amend [r2328]

--- plask/data.cpp
+++ plask/data.cpp
@@ -2,10 +2,10 @@
 
 namespace plask {
 
-template struct PLASK_API DataVector<double>;
+/*template struct PLASK_API DataVector<double>;
 template struct PLASK_API DataVector<const double>;
 template struct PLASK_API DataVector<std::complex<double>>;
-template struct PLASK_API DataVector<const std::complex<double>>;
+template struct PLASK_API DataVector<const std::complex<double>>;*/
 
 
 
