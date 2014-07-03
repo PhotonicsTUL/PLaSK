77627c8a3a7cf59504063b819d5bf906f5c3176e
piotr <piotr@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2014-07-03 21:36:27 +0000

    Some work on GUI [r2422]

--- gui/controller/grids/mesh_rectilinear.py
+++ gui/controller/grids/mesh_rectilinear.py
@@ -4,17 +4,21 @@ from ...qt import QtGui
 
 class AxisEdit(QtGui.QGroupBox):
 
-    def __init__(self, title, accept_points = False):
+    def __init__(self, title, accept_non_regular = False):
         super(AxisEdit, self).__init__(title)
-        self.start = QtGui.QLineEdit()
-        self.stop = QtGui.QLineEdit()
-        self.step = QtGui.QLineEdit()
         form_layout = QtGui.QFormLayout()
+        self.accept_non_regular = accept_non_regular
+        if self.accept_non_regular:
+            self.type = QtGui.QComboBox()
+            self.type.addItems(['', 'ordered', 'regular'])
+            form_layout.addRow("type", self.type)
+        self.start = QtGui.QLineEdit()
         form_layout.addRow("start", self.start)
+        self.stop = QtGui.QLineEdit()
         form_layout.addRow("stop", self.stop)
+        self.step = QtGui.QLineEdit()
         form_layout.addRow("step", self.step)
-        self.accept_points = accept_points
-        if self.accept_points:
+        if self.accept_non_regular:
             self.points = QtGui.QTextEdit()
             self.points.setWordWrapMode(QtGui.QTextEdit.WidgetWidth)
             form_layout.addRow("points", self.points)
@@ -25,13 +29,14 @@ class AxisEdit(QtGui.QGroupBox):
         axis_model.stop = self.stop.text()
         axis_model.step = self.step.text()
         if self.accept_points:
+            #axis_model.type = self.type.get
             axis_model.points = self.points.toPlainText()
 
 
-class RectilinearMeshConroller(Controller):
+class RectangularMeshConroller(Controller):
 
     def __init__(self, model):
-        super(RectilinearMeshConroller, self).__init__(model=model)
+        super(RectangularMeshConroller, self).__init__(model=model)
 
         self.form = QtGui.QGroupBox()
 
