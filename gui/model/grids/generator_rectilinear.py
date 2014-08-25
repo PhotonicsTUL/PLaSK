9060eeb8fbf0a9083f181acf1837273c5373f276
piotr <piotr@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2014-08-25 11:57:40 +0000

    Some code for model of rect. divide generator, ref. #99 [r2552]

---
+++ gui/model/grids/generator_rectilinear.py
@@ -0,0 +1,29 @@
+from lxml.etree import Element
+
+from ...utils.xml import AttributeReader
+from .grid import Grid
+
+class RefinementConf(object):
+    """Store refinement configuration of rectilinear generator"""
+
+    def __init__(self, axis_nr=None, object=None, path=None, at=None, by=None, every=None):
+        self.axis_nr = axis_nr
+        self.object = object
+        self.path = path
+        self.at = at
+        self.by = by
+        self.every = every
+
+    def get_XML_element(self):
+        res = Element('axis{}'.format(self.axis_nr))
+        for attr in ['object', 'path', 'at', 'by', 'every']:
+            a = getattr(self, attr, None)
+            if a is not None: res.attrib[attr] = a
+        return res
+
+    def set_from_XML(self, axis_element):
+        if axis_element is None: return
+        self.axis_nr = axis_element.tag[-1]
+        with AttributeReader(axis_element) as a:
+            for attr in ['object', 'path', 'at', 'by', 'every']:
+                setattr(self, attr, a.get(attr, None))
