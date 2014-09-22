7a21ccd22abb19c20f83acc94d52806f38dbc717
piotr <piotr@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2014-09-22 06:58:35 +0000

    Text editors changed model only when sth. has been edited, close #220 [r2618]

--- gui/controller/source.py
+++ gui/controller/source.py
@@ -10,6 +10,7 @@ class SourceEditController(Controller):
         Controller.__init__(self, document, model)
         self.fresh = False
         self.visible = False
+        self.edited = False
 
     def create_source_editor(self, parent=None):
         ed = QtGui.QTextEdit(parent)
@@ -17,7 +18,12 @@ class SourceEditController(Controller):
         self.highlighter = XMLHighlighter(ed.document())   # highlighter variable is required,
                                                            # in other case it is deleted and text is not highlighted
         ed.setReadOnly(self.model.is_read_only())
+        self.edited = False
         return ed
+
+    def __on_text_edit__(self):
+        self.edited = True
+        self.document.set_changed()
 
     # text, source editor
     def get_source_editor(self):
@@ -36,23 +42,24 @@ class SourceEditController(Controller):
             self.fresh = False
 
     def save_data_in_model(self):
-        if not self.get_source_editor().isReadOnly():
+        if not self.get_source_editor().isReadOnly() and self.edited:
             if hasattr(self.model, 'changed'): self.model.changed -= self.refresh_editor
             try:
                 self.model.set_text(self.get_source_editor().toPlainText())
             finally:
                 if hasattr(self.model, 'changed'): self.model.changed += self.refresh_editor
+                self.edited = False
 
     def on_edit_enter(self):
         self.visible = True
         if not self.fresh: self.refresh_editor()
         if hasattr(self.model, 'changed'): self.model.changed += self.refresh_editor
-        self.source_editor.textChanged.connect(self.document.set_changed)
+        self.source_editor.textChanged.connect(self.__on_text_edit__)
 
     # When the editor is turned off, model should be updated
     def on_edit_exit(self):
         try:
-            self.source_editor.textChanged.disconnect(self.document.set_changed)
+            self.source_editor.textChanged.disconnect(self.__on_text_edit__)
         except TypeError:
             pass
         self.save_data_in_model()
