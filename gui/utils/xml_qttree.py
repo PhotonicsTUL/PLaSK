48c17d98c9008ffa18fac8a63e670bb820f261f5
maciek <maciek@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2014-09-25 10:48:48 +0000

    Added GPL heading to all GUI files (we use some GPL components in GUI). [r2653]

--- gui/utils/xml_qttree.py
+++ gui/utils/xml_qttree.py
@@ -1,3 +1,15 @@
+# Copyright (C) 2014 Photonics Group, Lodz University of Technology
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the license, or (at your
+# opinion) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
 from ..qt import QtCore
 
 class ETreeModel(QtCore.QAbstractItemModel):
