418f349c13b46e49d02e75395121ae2aa0d2e4f3
maciek <maciek@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2014-10-31 23:02:00 +0000

    New PLaSK icon. Sorted-out GUI and program icons. [r2819]

--- toolset/find_icons.sh
+++ toolset/find_icons.sh
@@ -3,11 +3,16 @@ guidir=`dirname $0`/../gui
 icons=$(grep -r fromTheme ${guidir}/* | perl -ne "print \$2.\"\n\" if /fromTheme\(('|\")(.*)\\1\)/" | sort | uniq)
 
 for icon in ${icons}; do
-    for size in 16 24 32 64; do
-        iconfile=${guidir}/icons/hicolor/${size}x${size}/${icon}.png
+    for size in 16 24 32; do
+        iconfile=${guidir}/icons/hicolor/${size}x${size}/actions/${icon}.png
         if [ ! -f ${iconfile} ]; then
-            cp /usr/share/icons/Tango/${size}x${size}/*/${icon}.png ${iconfile} && \
+            cp /usr/share/icons/Tango/${size}x${size}/actions/${icon}.png ${iconfile} && \
             svn add ${iconfile}
         fi
     done
+    iconfile=${guidir}/icons/hicolor/scalable/actions/${icon}.svg
+    if [ ! -f ${iconfile} ]; then
+        cp /usr/share/icons/Tango/scalable/actions/${icon}.svg ${iconfile} && \
+        svn add ${iconfile}
+    fi
 done
