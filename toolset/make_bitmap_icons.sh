2d2b8da540b4104e10c04ca4e669af260482dc8f
maciek <maciek@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2014-11-01 13:43:10 +0000

    Utils improvement. [r2821]

--- toolset/make_bitmap_icons.sh
+++ toolset/make_bitmap_icons.sh
@@ -1,7 +1,20 @@
-cd gui/icons/hicolor/
-for s in 24 32 48; do inkscape {s}x${s}/apps/plask.png scalable/apps/plask.svg -C -w$s -h$s; done
-for s in 24 32 48; do inkscape -e {s}x${s}/apps/plask.png scalable/apps/plask.svg -C -w $s -h $s; done
-for s in 24 32 48; do inkscape -e ${s}x${s}/apps/plask.png scalable/apps/plask.svg -C -w $s -h $s; done
-for s in 16 24 32 48; do inkscape -e ${s}x${s}/mimetypes/application-x-plask.svg scalable/mimetypes/application-x-plask.svg -C -w $s -h $s; done
-for s in 16 32 64 128 256; do inkscape -e ../../../utils/xpl-${s}.png scalable/mimetypes/application-x-plask.svg -C -w $s -h $s; done
-for s in 16 32 64 128 256; do inkscape -e ../../../utils/plask-${s}.png scalable/apps/plask.svg -C -w $s -h $s; done
+#!/bin/sh
+iconsdir=`dirname $0`/../gui/icons/hicolor
+utilsdir=`dirname $0`/../utils
+
+for s in 24 32 48; do
+    inkscape -e ${iconsdir}/${s}x${s}/apps/plask.png ${iconsdir}/scalable/apps/plask.svg -C -w $s -h $s
+    inkscape -e ${iconsdir}/${s}x${s}/mimetypes/application-x-plask.png \
+        ${iconsdir}/scalable/mimetypes/application-x-plask.svg -C -w ${s} -h ${s}
+done
+inkscape -e ${iconsdir}/16x16/mimetypes/application-x-plask.svg \
+    ${iconsdir}/scalable/mimetypes/application-x-plask.svg -C -w 16 -h 16
+
+for s in 16 32 64 128 256; do
+    inkscape -e /tmp/plask-${s}.png ${iconsdir}/scalable/apps/plask.svg -C -w ${s} -h ${s}
+    inkscape -e /tmp/xpl-${s}.png ${iconsdir}/scalable/mimetypes/application-x-plask.svg -C -w ${s} -h ${s}
+done
+convert /tmp/plask-16.png /tmp/plask-32.png /tmp/plask-64.png /tmp/plask-128.png /tmp/plask-256.png ${utilsdir}/plask.ico
+convert /tmp/xpl-16.png /tmp/xpl-32.png /tmp/xpl-64.png /tmp/xpl-128.png /tmp/xpl-256.png ${utilsdir}/xpl.ico
+rm /tmp/plask-16.png /tmp/plask-32.png /tmp/plask-64.png /tmp/plask-128.png /tmp/plask-256.png
+rm /tmp/xpl-16.png /tmp/xpl-32.png /tmp/xpl-64.png /tmp/xpl-128.png /tmp/xpl-256.png
