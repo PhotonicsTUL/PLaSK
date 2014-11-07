8f31ec4ef290e3688d1ef09b3ac1c88de63f59a1
piotr <piotr@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2014-11-07 13:47:03 +0000

    Support for position in container settings in geometry GUI, ref. #222 [r2857]

--- gui/controller/geometry/object.py
+++ gui/controller/geometry/object.py
@@ -18,18 +18,29 @@ from ...utils.str import empty_to_none, none_to_empty
 
 class GNObjectController(GNodeController):
 
+    #def __init__(self, document, model, node):
+    #    super(GNObjectController, self).__init__(document, model, node)
+    #    self.in_parent_controller = self.node.get_controller_for_inparent()
+
     def fill_form(self):
         self.construct_group('Basic settings')
         self.name = self.construct_line_edit('name')
         self.role = self.construct_line_edit('roles')
         self.axes = self.construct_combo_box('axes', ['', 'x, y, z', 'z, x, y', 'p, r, z', 'l, t, v', 'long, tran, vert'])   #TODO zablokować możliwość podawania {
+        self.in_parent_controller = self.node.get_controller_for_inparent()
+        if self.in_parent_controller is not None:
+            self.vbox.insertWidget(0, self.in_parent_controller.get_widget())
+            #self.construct_group('Position in container', position=0)
+            #self._current_form.addChildWidget(in_parent_widget)
 
     def save_data_in_model(self):
         self.node.name = empty_to_none(self.name.text())
         self.node.role = empty_to_none(self.role.text())
         self.node.axes = empty_to_none(self.axes.currentText())
+        if self.in_parent_controller is not None: self.in_parent_controller.save_data_in_model()
 
     def on_edit_enter(self):
         self.name.setText(none_to_empty(self.node.name))
         self.role.setText(none_to_empty(self.node.role))
         self.axes.setEditText(axes_to_str(self.node.axes))
+        if self.in_parent_controller is not None: self.in_parent_controller.on_edit_enter()
