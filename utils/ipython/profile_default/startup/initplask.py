ea157888574c9b06a22db5a215262c1ebdfb44e5
maciek <maciek@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2015-08-13 13:39:13 +0000

    Amend. [r3382]

--- utils/ipython/profile_default/startup/initplask.py
+++ utils/ipython/profile_default/startup/initplask.py
@@ -3,8 +3,9 @@ from __future__ import division
 import plask
 from plask import *
 
+olask.config.log.use_python()
 plask.config.log.output = "stdout"
-plask.config.log.coloring = "ansi"
+plask.config.log.colors = "ansi"
 plask.__globals = globals()
 
 #switch_backend("module://IPython.zmq.pylab.backend_inline")
