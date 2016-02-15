bc16850e194b5902c7741875f274e2b8356a1bba
piotr <piotr@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2016-02-15 23:16:03 +0000

    Amend, ref #400 [r3857]

--- plask/vector/3d.h
+++ plask/vector/3d.h
@@ -413,11 +413,6 @@ inline constexpr Vec<3,T> vec(const T c0__lon, const T c1__tran, const T c2__up)
 PLASK_API_EXTERN_TEMPLATE_STRUCT(Vec<3, double>)
 PLASK_API_EXTERN_TEMPLATE_STRUCT(Vec<3, std::complex<double> >)
 
-#if defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
-extern template struct PLASK_API Vec<3, double>;
-extern template struct PLASK_API Vec<3, std::complex<double> >;
-#endif
-
 } //namespace plask
 
 #endif // PLASK__VECTORCART3D_H
