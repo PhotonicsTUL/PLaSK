f71cdbb465b54fa05fa0685849963b08bf4f1949
piotr <piotr@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2016-02-16 21:36:54 +0000

    Potential fix for fftpack crash (stack overflow?), fixed compilation under MSVC, ref #400 [r3873]

--- solvers/optical/slab/fourier/fftpack.cpp
+++ solvers/optical/slab/fourier/fftpack.cpp
@@ -49,21 +49,21 @@ void Forward1D::execute(dcomplex* data) {
     if (!wsave) throw CriticalException("FFTPACX not initialized");
     try {
         int ier;
-        double work[2*lot*(n+1)];
+        std::unique_ptr<double[]> work(new double[2*lot*(n+1)]);
         double factor;
         switch (symmetry) {
             case (SYMMETRY_NONE):
-                cfftmf_(lot, 1, n, strid, data, strid*n, wsave, lensav(n), work, 2*lot*n, ier);
+                cfftmf_(lot, 1, n, strid, data, strid*n, wsave, lensav(n), work.get(), 2*lot*n, ier);
                 break;
             case (SYMMETRY_EVEN_2):
-                cosqmb_(2*lot, 1, n, 2*strid, (double*)data, 2*strid*n, wsave, lensav(n), work, 2*lot*n, ier);
+                cosqmb_(2*lot, 1, n, 2*strid, (double*)data, 2*strid*n, wsave, lensav(n), work.get(), 2*lot*n, ier);
                 factor = 1./n;
                 for (int i = 0, N = strid*n; i < N; i += strid)
                     for (int j = 0; j < lot; ++j)
                         data[i+j] *= factor;
                 break;
             case (SYMMETRY_EVEN_1):
-                costmf_(2*lot, 1, n, 2*strid, (double*)data, 2*strid*n, wsave, lensav(n), work, 2*lot*(n+1), ier);
+                costmf_(2*lot, 1, n, 2*strid, (double*)data, 2*strid*n, wsave, lensav(n), work.get(), 2*lot*(n+1), ier);
                 for (int i = lot, end = n*lot; i < end; ++i) *(data+i) *= 0.5;
                 break;
             default: {} // silence the warning
@@ -123,20 +123,20 @@ void Backward1D::execute(dcomplex* data) {
     if (!wsave) throw CriticalException("FFTPACX not initialized");
     try {
         int ier;
-        double work[(symmetry==SYMMETRY_ODD_1)? 4*lot*n : 2*lot*(n+1)];
+        std::unique_ptr<double[]> work(new double[(symmetry==SYMMETRY_ODD_1)? 4*lot*n : 2*lot*(n+1)]);
         switch (symmetry) {
             case SYMMETRY_NONE:
-                cfftmb_(lot, 1, n, strid, data, strid*n, wsave, lensav(n), work, 2*lot*n, ier);
+                cfftmb_(lot, 1, n, strid, data, strid*n, wsave, lensav(n), work.get(), 2*lot*n, ier);
                 return;
             case SYMMETRY_EVEN_2:
-                cosqmf_(2*lot, 1, n, 2*strid, (double*)data, 2*strid*n, wsave, lensav(n), work, 2*lot*n, ier);
+                cosqmf_(2*lot, 1, n, 2*strid, (double*)data, 2*strid*n, wsave, lensav(n), work.get(), 2*lot*n, ier);
                 break;
             case SYMMETRY_ODD_2:
-                sinqmf_(2*lot, 1, n, 2*strid, (double*)data, 2*strid*n, wsave, lensav(n), work, 2*lot*n, ier);
+                sinqmf_(2*lot, 1, n, 2*strid, (double*)data, 2*strid*n, wsave, lensav(n), work.get(), 2*lot*n, ier);
                 break;
             case SYMMETRY_EVEN_1:
                 for (int i = lot, end = n*lot; i < end; ++i) *(data+i) *= 2.;
-                costmb_(2*lot, 1, n, 2*strid, (double*)data, 2*strid*n, wsave, lensav(n), work, 2*lot*(n+1), ier);
+                costmb_(2*lot, 1, n, 2*strid, (double*)data, 2*strid*n, wsave, lensav(n), work.get(), 2*lot*(n+1), ier);
                 return;
             default: {}
         }
@@ -212,17 +212,17 @@ void Forward2D::execute(dcomplex* data) {
     if (!wsave1 || !wsave2) throw CriticalException("FFTPACX not initialized");
     try {
         int ier;
-        double work[2*lot*(max(n1,n2)+1)];
+		std::unique_ptr<double[]> work(new double[2*lot*(max(n1,n2)+1)]);
         // n1 is changing faster than n2
         double factor1 = 1./n1;
         switch (symmetry1) {
             case (SYMMETRY_NONE):
                 for (int i = 0; i != n2; ++i)
-                    cfftmf_(lot, 1, n1, strid1, data+strid2*i, strid2, wsave1, lensav(n1), work, 2*lot*n1, ier);
+                    cfftmf_(lot, 1, n1, strid1, data+strid2*i, strid2, wsave1, lensav(n1), work.get(), 2*lot*n1, ier);
                 break;
             case (SYMMETRY_EVEN_2):
                 for (int i = 0; i != n2; ++i) {
-                    cosqmb_(2*lot, 1, n1, 2*strid1, (double*)data+2*strid2*i, 2*strid2, wsave1, lensav(n1), work, 2*lot*n1, ier);
+                    cosqmb_(2*lot, 1, n1, 2*strid1, (double*)data+2*strid2*i, 2*strid2, wsave1, lensav(n1), work.get(), 2*lot*n1, ier);
                     for (int j = 0, dist = strid2*i, end = strid1*n1; j < end; j += strid1)
                         for (int l = 0; l < lot; ++l)
                             data[dist+j+l] *= factor1;
@@ -230,7 +230,7 @@ void Forward2D::execute(dcomplex* data) {
                 break;
             case (SYMMETRY_EVEN_1):
                 for (int i = 0; i != n2; ++i) {
-                    costmf_(2*lot, 1, n1, 2*strid1, (double*)data+2*strid2*i, 2*strid2, wsave1, lensav(n1), work, 2*lot*(n1+1), ier);
+                    costmf_(2*lot, 1, n1, 2*strid1, (double*)data+2*strid2*i, 2*strid2, wsave1, lensav(n1), work.get(), 2*lot*(n1+1), ier);
                     for (int j = strid1, dist = strid2*i, end = strid1*n1; j < end; j += strid1)
                         for (int l = 0; l < lot; ++l)
                             data[dist+j+l] *= 0.5;
@@ -242,11 +242,11 @@ void Forward2D::execute(dcomplex* data) {
         switch (symmetry2) {
             case (SYMMETRY_NONE):
                 for (int i = 0; i != n1; ++i)
-                    cfftmf_(lot, 1, n2, strid2, data+strid1*i, strid1+strid2*(n2-1), wsave2, lensav(n2), work, 2*lot*n2, ier);
+                    cfftmf_(lot, 1, n2, strid2, data+strid1*i, strid1+strid2*(n2-1), wsave2, lensav(n2), work.get(), 2*lot*n2, ier);
                 break;
             case (SYMMETRY_EVEN_2):
                 for (int i = 0; i != n1; ++i) {
-                    cosqmb_(2*lot, 1, n2, 2*strid2, (double*)data+2*strid1*i, 2*(strid1+strid2*(n2-1)), wsave2, lensav(n2), work, 2*lot*n2, ier);
+                    cosqmb_(2*lot, 1, n2, 2*strid2, (double*)data+2*strid1*i, 2*(strid1+strid2*(n2-1)), wsave2, lensav(n2), work.get(), 2*lot*n2, ier);
                     for (int j = 0, dist = strid1*i, end = n2*strid2; j < end; j += strid2)
                         for (int l = 0; l < lot; ++l)
                             data[dist+j+l] *= factor2;
@@ -254,7 +254,7 @@ void Forward2D::execute(dcomplex* data) {
                 break;
             case (SYMMETRY_EVEN_1):
                 for (int i = 0; i != n1; ++i) {
-                    costmf_(2*lot, 1, n2, 2*strid2, (double*)data+2*strid1*i, 2*(strid1+strid2*(n2-1)), wsave2, lensav(n2), work, 2*lot*(n2+1), ier);
+                    costmf_(2*lot, 1, n2, 2*strid2, (double*)data+2*strid1*i, 2*(strid1+strid2*(n2-1)), wsave2, lensav(n2), work.get(), 2*lot*(n2+1), ier);
                     for (int j = strid2, dist = strid1*i, end = strid2*n2; j < end; j += strid2)
                         for (int l = 0; l < lot; ++l)
                             data[dist+j+l] *= 0.5;
@@ -336,17 +336,17 @@ void Backward2D::execute(dcomplex* data) {
     if (!wsave1 || !wsave2) throw CriticalException("FFTPACX not initialized");
     try {
         int ier;
-        double work[2*lot*(max(n1,n2)+1)];
+		std::unique_ptr<double[]> work(new double[2*lot*(max(n1,n2)+1)]);
         // n1 is changing faster than n2
         double factor1 = n1;
         switch (symmetry1) {
             case (SYMMETRY_NONE):
                 for (int i = 0; i != n2; ++i)
-                    cfftmb_(lot, 1, n1, strid1, data+strid2*i, strid2, wsave1, lensav(n1), work, 2*lot*n1, ier);
+                    cfftmb_(lot, 1, n1, strid1, data+strid2*i, strid2, wsave1, lensav(n1), work.get(), 2*lot*n1, ier);
                 break;
             case (SYMMETRY_EVEN_2):
                 for (int i = 0; i != n2; ++i) {
-                    cosqmf_(2*lot, 1, n1, 2*strid1, (double*)data+2*strid2*i, 2*strid2, wsave1, lensav(n1), work, 2*lot*n1, ier);
+                    cosqmf_(2*lot, 1, n1, 2*strid1, (double*)data+2*strid2*i, 2*strid2, wsave1, lensav(n1), work.get(), 2*lot*n1, ier);
                     for (int j = 0, dist = strid2*i, end = strid1*n1; j < end; j += strid1)
                         for (int l = 0; l < lot; ++l)
                             data[j+l+dist] *= factor1;
@@ -354,7 +354,7 @@ void Backward2D::execute(dcomplex* data) {
                 break;
             case (SYMMETRY_ODD_2):
                 for (int i = 0; i != n2; ++i) {
-                    sinqmf_(2*lot, 1, n1, 2*strid1, (double*)data+2*strid2*i, 2*strid2, wsave1, lensav(n1), work, 2*lot*n1, ier);
+                    sinqmf_(2*lot, 1, n1, 2*strid1, (double*)data+2*strid2*i, 2*strid2, wsave1, lensav(n1), work.get(), 2*lot*n1, ier);
                     for (int j = 0, dist = strid2*i, end = strid1*n1; j < end; j += strid1)
                         for (int l = 0; l < lot; ++l)
                             data[j+l+dist] *= factor1;
@@ -365,7 +365,7 @@ void Backward2D::execute(dcomplex* data) {
                     for (int j = strid1, dist = strid2*i, end = strid1*n1; j < end; j += strid1)
                         for (int l = 0; l < lot; ++l)
                             data[j+l+dist] *= 2.;
-                    costmb_(2*lot, 1, n1, 2*strid1, (double*)data+2*strid2*i, 2*strid2, wsave1, lensav(n1), work, 2*lot*(n1+1), ier);
+                    costmb_(2*lot, 1, n1, 2*strid1, (double*)data+2*strid2*i, 2*strid2, wsave1, lensav(n1), work.get(), 2*lot*(n1+1), ier);
                 }
                 break;
             default: {}
@@ -374,11 +374,11 @@ void Backward2D::execute(dcomplex* data) {
         switch (symmetry2) {
             case (SYMMETRY_NONE):
                 for (int i = 0; i != n1; ++i)
-                    cfftmb_(lot, 1, n2, strid2, data+strid1*i, strid1+strid2*(n2-1), wsave2, lensav(n2), work, 2*lot*n2, ier);
+                    cfftmb_(lot, 1, n2, strid2, data+strid1*i, strid1+strid2*(n2-1), wsave2, lensav(n2), work.get(), 2*lot*n2, ier);
                 break;
             case (SYMMETRY_EVEN_2):
                 for (int i = 0; i != n1; ++i) {
-                    cosqmf_(2*lot, 1, n2, 2*strid2, (double*)data+2*strid1*i, 2*(strid1+strid2*(n2-1)), wsave2, lensav(n2), work, 2*lot*n2, ier);
+                    cosqmf_(2*lot, 1, n2, 2*strid2, (double*)data+2*strid1*i, 2*(strid1+strid2*(n2-1)), wsave2, lensav(n2), work.get(), 2*lot*n2, ier);
                     for (int j = 0, dist = strid1*i, N = n2*strid2; j < N; j += strid2)
                         for (int l = 0; l < lot; ++l)
                             data[dist+j+l] *= factor2;
@@ -386,7 +386,7 @@ void Backward2D::execute(dcomplex* data) {
                 break;
             case (SYMMETRY_ODD_2):
                 for (int i = 0; i != n1; ++i) {
-                    sinqmf_(2*lot, 1, n2, 2*strid2, (double*)data+2*strid1*i, 2*(strid1+strid2*(n2-1)), wsave2, lensav(n2), work, 2*lot*n2, ier);
+                    sinqmf_(2*lot, 1, n2, 2*strid2, (double*)data+2*strid1*i, 2*(strid1+strid2*(n2-1)), wsave2, lensav(n2), work.get(), 2*lot*n2, ier);
                     for (int j = 0, dist = strid1*i, N = n2*strid2; j < N; j += strid2)
                         for (int l = 0; l < lot; ++l)
                             data[dist+j+l] *= factor2;
@@ -397,7 +397,7 @@ void Backward2D::execute(dcomplex* data) {
                     for (int j = strid2, dist = strid1*i, end = n2*strid2; j < end; j += strid2)
                         for (int l = 0; l < lot; ++l)
                             data[dist+j+l] *= 2.;
-                    costmb_(2*lot, 1, n2, 2*strid2, (double*)data+2*strid1*i, 2*(strid1+strid2*(n2-1)), wsave2, lensav(n2), work, 2*lot*(n2+1), ier);
+                    costmb_(2*lot, 1, n2, 2*strid2, (double*)data+2*strid1*i, 2*(strid1+strid2*(n2-1)), wsave2, lensav(n2), work.get(), 2*lot*(n2+1), ier);
                 }
                 break;
             default: {}
