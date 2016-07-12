adc8b75ae9f66d5bc0363521619468b9e1c88ec2
maciek <maciek@b48bb66f-033f-47dd-ae7b-7d17f75e9aab>
2016-07-12 08:03:54 +0000

    Manifest in Widows is optional. [r4024]

--- toolset/mingw/manifest_mingw32.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python create_manifest.py
-i686-pc-mingw32-windres --input msvcr_exe.rc --output msvcrc_exe.o
-i686-pc-mingw32-windres --input msvcr_dll.rc --output msvcrc_dll.o
-
-i686-pc-mingw32-gcc -dumpspecs | sed -e '/\*cpp:/ {n; s/^/-D__MSVCRT_VERSION__=0x0900 /}' -e 's/-lmoldname/-lmoldname90/' -e 's/-lmsvcrt/-lmsvcr90/' > specs
