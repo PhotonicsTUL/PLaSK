9bf0d07703a02f4875c6642250788eff28f7a509
Paweł Gliwny <p.gliwny@gmail.com>
2018-02-28 12:09:39 +0100

    improved compute transfer matrix

--- solvers/optical/simple_optical/simple_optical.cpp
+++ solvers/optical/simple_optical/simple_optical.cpp
@@ -8,10 +8,10 @@ SimpleOptical::SimpleOptical(const std::string& name):plask::SolverOver<plask::G
   ,stripex(0)
 {
   stripe_root.method = RootDigger::ROOT_MULLER;
-  stripe_root.tolx = 1.0e-6;
+  stripe_root.tolx = 1.0e-8;
   stripe_root.tolf_min = 1.0e-7;
-  stripe_root.tolf_max = 1.0e-5;
-  stripe_root.maxiter = 500;
+  stripe_root.tolf_max = 1.0e-8;
+  stripe_root.maxiter = 1000;
 }
 
 void SimpleOptical::loadConfiguration(XMLReader& reader, Manager& manager) {
@@ -50,26 +50,24 @@ void SimpleOptical::onInitialize()
     axis_midpoints_horizontal = midpoints->ee_x();
     ybegin = 0;
     yend = mesh->axis1->size() + 1;
+    //edgeVertLayerPoint.push_back(-1e-3);
     for (double p: *axis_vertical) edgeVertLayerPoint.push_back(p);  
+    double last_element = edgeVertLayerPoint.back();
+    edgeVertLayerPoint.push_back(last_element+1e-3);
+    
 }
 
 
 void SimpleOptical::initializeRefractiveIndexVec()
 {
-   nrCache.clear();
-  double T = 300; //temperature 300 K
-  double w = real(2e3*M_PI / k0);
-  nrCache.push_back(geometry->getMaterial(vec(double(stripex), -1e-3))->Nr(w, T));
-  for(double p: *axis_midpoints_vertical) 
-  {
-    nrCache.push_back(geometry->getMaterial(vec(double(stripex),  p))->Nr(w, T));
-  }
-  double last_element = 0;
-  last_element = edgeVertLayerPoint.back();
-  nrCache.push_back(geometry->getMaterial(vec(double(stripex),  last_element+1e-3))->Nr(w, T));
+    nrCache.clear();
+    double T = 300; //temperature 300 K
+    double w = real(2e3*M_PI / k0);
+    nrCache.push_back(geometry->getMaterial(vec(double(stripex), -1e-3))->Nr(w, T));
+    for(double p: *axis_midpoints_vertical) {
+    nrCache.push_back(geometry->getMaterial(vec(double(stripex),  p))->Nr(w, T));}
+    nrCache.push_back(geometry->getMaterial(vec(double(stripex),  edgeVertLayerPoint.back()))->Nr(w, T));
 }
-
-
 
 size_t SimpleOptical::findMode(double lambda, int m)
 {
@@ -101,19 +99,24 @@ dcomplex SimpleOptical::computeTransferMatrix(const dcomplex& x, const std::vect
     Matrix transfer_matrix = Matrix::eye();
     dcomplex phas;
   
+    vecE.clear();
     FieldZ field(0,1);
     vecE.push_back(field);
-    for (size_t i = ybegin; i<yend-1; ++i)
+    
+    for (size_t i = ybegin; i<yend; ++i)
     {
-        if (i != ybegin || ybegin != 0) d = edgeVertLayerPoint[i] - edgeVertLayerPoint[i-1]; 
-        else d = 0.;
+    if (i != ybegin || ybegin != 0) d = edgeVertLayerPoint[i] - edgeVertLayerPoint[i-1]; 
+    else d = 0.;
+    std::cout<<"edge layer point = " << edgeVertLayerPoint[i] << std::endl;
     phas_matrix = Matrix(exp(I*NR[i]*x*d), 0, 0, exp(-I*NR[i]*x*d));
     boundary_matrix = Matrix( 0.5+0.5*(NR[i]/NR[i+1]), 0.5-0.5*(NR[i]/NR[i+1]),
                               0.5-0.5*(NR[i]/NR[i+1]), 0.5+0.5*(NR[i]/NR[i+1]) );
     transfer_matrix = (boundary_matrix*phas_matrix)*transfer_matrix;       
     FieldZ Ei = vecE[i]*(boundary_matrix*phas_matrix);
-    vecE.push_back(Ei);}   
- 
+    vecE.push_back(Ei);
+    std::cout<<"B[i]="<<vecE.back().B<<"   F[i]= "<<vecE.back().F<<std::endl;
+    }   
+    std::cout<<"Vec back = " << vecE.back().B << std::endl;
     return transfer_matrix.bb;
 }
 
@@ -150,73 +153,78 @@ const LazyData<Tensor3<dcomplex>> SimpleOptical::getRefractiveIndex(const shared
 
 const DataVector<double> SimpleOptical::getLightMagnitude(int num, const shared_ptr<const MeshD<2>>& dst_mesh, InterpolationMethod)
 {
-  setWavelength(real(modes[num].lam));  
-  initCalculation();
-  vecE.clear();
-  computeTransferMatrix(k0, nrCache);
+    setWavelength(real(modes[num].lam));  
+    initCalculation();
+    vecE.clear();
+    std::cout<<"Vec E size = " << vecE.size() << std::endl;
+    computeTransferMatrix(k0, nrCache);
   
-  std::vector<double> arrayZ;  
-  for (auto v: *dst_mesh) {
-    double z = v.c1;
-    arrayZ.push_back(z);
-  }
+    std::vector<double> arrayZ;  
+    for (auto v: *dst_mesh) {
+     double z = v.c1;
+     arrayZ.push_back(z);}
  
-  DataVector<double> results(arrayZ.size());
-  std::vector<dcomplex> NR;
-  std::vector<double> verticalEdgeVec;
-  std::vector<double> hi;
-  std::vector<dcomplex> B;
-  std::vector<dcomplex> F;
-  double T = 300; //temperature 300 K
-  double w = real(2e3*M_PI / k0);
+    DataVector<double> results(arrayZ.size());
+    std::vector<dcomplex> NR;
+    std::vector<double> verticalEdgeVec;
+    std::vector<double> hi;
+    std::vector<dcomplex> B;
+    std::vector<dcomplex> F;
+    double T = 300; //temperature 300 K
+    double w = real(2e3*M_PI / k0);
   
-  for(auto p: arrayZ) 
-  {
-    NR.push_back(geometry->getMaterial(vec(double(stripex),  p))->Nr(w, T));      
-  }
-  //MD: dlaczego nie korzysta Pan z `nrCache`?
-
-  for (double p_edge: *axis_vertical) verticalEdgeVec.push_back(p_edge);
-
-  for (size_t i = 0; i < verticalEdgeVec.size()-1; ++i)
-  {
-    for (double p: arrayZ) 
+    for(auto p: arrayZ) 
     {
-
+      NR.push_back(geometry->getMaterial(vec(double(stripex),  p))->Nr(w, T));      
+    }
+    //MD: dlaczego nie korzysta Pan z `nrCache`?
+     
+    
+    
+    
+    
+    for (double p_edge: *axis_vertical) verticalEdgeVec.push_back(p_edge);
+    std::cout<<"Vec E size = " << vecE.size() << std::endl;
+    for (size_t i = 0; i < verticalEdgeVec.size()-1; ++i)
+    {
+     for (double p: arrayZ) 
+     {
       if (verticalEdgeVec[i] <= p and verticalEdgeVec[i+1] > p)
       {
         hi.push_back(p - verticalEdgeVec[i]);        
         B.push_back(vecE[i+1].B);
-        F.push_back(vecE[i+1].F);        
+        
+        F.push_back(vecE[i+1].F);
+        //std::cout<<"B[i]="<<B[i]<<"   hi[i]="<<hi[i]<<"   F[i]= "<<F[i]<<std::endl;}      
       }
     }
-   }
-   
-   hi.push_back(0);
-   B.push_back(0);
-   F.push_back(vecE.back().F);
-   
-   for(double p: arrayZ) // propagation wave after escape from structure 
-   {
-   if (p > verticalEdgeVec.back())
-   {
-       
-    hi.push_back(p-verticalEdgeVec.back());    
-    B.push_back(0);
-    F.push_back(vecE.back().F);
     }
-   } 
-   dcomplex Ez;
-   for (size_t i = 0; i < hi.size(); ++i)
-   {
-      Ez = F[i]*exp(-I*NR[i]*k0*hi[i]) + B[i]*exp(I*NR[i]*k0*hi[i]); 
-      results[i] = real(Ez*conj(Ez));
-      //writelog(LOG_INFO, "F  = {0}, B = {1}", str(F[i]), str(B[i]) );
-      std::cout<<"B[i]="<<B[i]<<"   hi[i]="<<hi[i]<<"F[i]= "<<F[i]<<std::endl;
-   }
-   
-   return results;
+     //hi.push_back(0);
+    
+    std::cout<<"Wave Escape !!!!!!!!!" << std::endl;
+    for(double p: arrayZ) // propagation wave after escape from structure 
+    {
+    if (p > verticalEdgeVec.back())
+    {   
+    hi.push_back(p-verticalEdgeVec.back());    
+    B.push_back(vecE.back().B);
+    F.push_back(vecE.back().F);
+    std::cout<<"B[i]="<<vecE.back().B<<"   F[i]= "<<vecE.back().F<<std::endl;
+    }} 
+    
+    
+    dcomplex Ez;
+    for (size_t i = 0; i < hi.size(); ++i)
+    {
+       Ez = F[i]*exp(-I*NR[i]*k0*hi[i]) + B[i]*exp(I*NR[i]*k0*hi[i]); 
+       results[i] = real(Ez*conj(Ez));
+       
+       //writelog(LOG_INFO, "F  = {0}, B = {1}", str(F[i]), str(B[i]) );
+       std::cout<<"B[i]="<<B[i]<<"   hi[i]="<<hi[i]<<"   F[i]= "<<F[i]<<std::endl;
+    }   
+    return results;
 }
+
 
 
 
