6c9e44993574ee07c093e95c6a16ad105e7fccfd
Maciej Dems <maciej.dems@p.lodz.pl>
2018-04-16 10:39:26 +0200

    Appcast update

--- utils/appcast/plask.py
+++ utils/appcast/plask.py
@@ -26,29 +26,30 @@ def print_enclosure(no, name, os_arch, dist, sep='-'):
 	pubdate = time.ctime(os.path.getctime(filepath))
 	length = os.path.getsize(filepath)
 	print """    <item>
-	  <title>{name} {ver}</title>
-	  <pubDate>{pubdate} +0000</pubDate>""".format(**locals())
+	<title>{name} {ver}</title>
+	<pubDate>{pubdate} +0000</pubDate>""".format(**locals())
 	if dist is not None:
-	    dist_info = '\n        sparkle:dist="{}"'.format(dist)
+	    dist_info = '\n            sparkle:dist="{}"'.format(dist)
 	else:
 	    dist_info = ''
-	print """      <enclosure
+	print """        <enclosure
 	    url="https://phys.p.lodz.pl/redmine/attachments/download/{no}/{source}"
 	    length="{length}"
 	    sparkle:version="{ver}"
 	    sparkle:os="{os_arch}"{dist_info}
-	    type="application/octet-stream"/>
-	</item>""".format(**locals())
+	    type="application/octet-stream"
+        />
+    </item>""".format(**locals())
     except Exception as err:
 	print "    <!-- {}: {} -->".format(name, err)
 
-print_enclosure(341, 'plask_{}_wily_amd64.deb', 'linux-x64', 'ubuntu-wily', sep='_')
-print_enclosure(112, 'plask_{}_trusty_amd64.deb', 'linux-x64', 'ubuntu-trusty', sep='_')
+print_enclosure(508, 'PLaSK-{}-win64.exe', 'windows-x64', None)
+print_enclosure(495, 'plask_{}_artful_amd64.deb', 'linux-x64', 'ubuntu-artful', sep='_')
+print_enclosure(112, 'plask_{}_zesty_amd64.deb', 'linux-x64', 'ubuntu-zesty', sep='_')
+print_enclosure(341, 'plask_{}_xenial_amd64.deb', 'linux-x64', 'ubuntu-xenial', sep='_')
 print_enclosure(168, 'plask-{}-rhel7.x86_64.rpm', 'linux-x64', 'redhat-7')
 print_enclosure(168, 'plask-{}-rhel7.x86_64.rpm', 'linux-x64', 'centos-7')
 print_enclosure(91, 'plask_{}_debian_amd64.deb', 'linux-x64', 'debian-8', sep='_')
-print_enclosure(4, 'PLaSK-{}-win64.exe', 'windows-x64', None)
-print_enclosure(4, 'PLaSK-{}-win64.exe', 'windows', None)
 
 #print """      <sparkle:version>{ver}</sparkle:version>
 #      <link>https://phys.p.lodz.pl/redmine/projects/plask/files/</link>""".format(ver=ver)
