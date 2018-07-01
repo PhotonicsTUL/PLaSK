8134fb3987f32de273457c279b483864f0f2e038
Maciej Dems <maciej.dems@p.lodz.pl>
2018-07-01 22:00:45 +0300

    PLaSK vesion can be specified manually (skipbuild)

--- toolset/package_version.py
+++ toolset/package_version.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
 import re, sys
-vre = re.compile('#define PLASK_VERSION "(\d{4}\.\d{2}\.\d{2})\.[\da-f]+')
+vre = re.compile(r'#define PLASK_VERSION "(.+)\.[\da-f]+"')
 m = vre.search(open('include/plask/version.h').read())
 if m is not None: sys.stdout.write(m.group(1))
