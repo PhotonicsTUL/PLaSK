a77951c0987d01bc80dba0e761beb4c0b75937c8
Piotr Beling <pbeling@emapa.pl>
2018-09-10 16:09:02 +0200

    work on triangular mesh

--- plask/mesh/triangular2d.cpp
+++ plask/mesh/triangular2d.cpp
@@ -2,6 +2,23 @@
 
 namespace plask {
 
+TriangularMesh2D::Builder::Builder(TriangularMesh2D &mesh): mesh(mesh) {
+    for (std::size_t i = 0; i < mesh.nodes.size(); ++i)
+        this->indexOfNode[mesh.nodes[i]] = i;
+}
+
+TriangularMesh2D::Builder &TriangularMesh2D::Builder::add(TriangularMesh2D::LocalCoords p1, TriangularMesh2D::LocalCoords p2, TriangularMesh2D::LocalCoords p3) {
+    mesh.elementsNodes.push_back({addNode(p1), addNode(p2), addNode(p3)});
+    return *this;
+}
+
+std::size_t TriangularMesh2D::Builder::addNode(TriangularMesh2D::LocalCoords node) {
+    auto it = this->indexOfNode.emplace(node, mesh.nodes.size());
+    if (it.second) // new element has been appended to the map
+        this->mesh.nodes.push_back(node);
+    return it.first->second;    // index of node (inserted or found)
+}
+
 
 
 }   // namespace plask
--- plask/mesh/triangular2d.h
+++ plask/mesh/triangular2d.h
@@ -8,7 +8,12 @@ namespace plask {
 
 struct TriangularMesh2D: public MeshD<2> {
 
-    std::vector< Vec<2, double> > nodes;
+    using MeshD<2>::LocalCoords;
+    typedef std::vector<LocalCoords> LocalCoordsVec;
+    typedef LocalCoordsVec::iterator iterator;
+    typedef LocalCoordsVec::const_iterator const_iterator;
+
+    LocalCoordsVec nodes;
 
     typedef std::array<std::size_t, 3> TriangleNodeIndexes;
 
@@ -18,12 +23,93 @@ struct TriangularMesh2D: public MeshD<2> {
         TriangleNodeIndexes triangleNodes;
         TriangularMesh2D& mesh;   // for getting access to the nodes
 
-        Vec<2, double> getNode(std::size_t index) const {
-            return mesh.nodes[index];
+        Element(TriangularMesh2D& mesh, TriangleNodeIndexes triangleNodes)
+            : triangleNodes(triangleNodes), mesh(mesh) {}
+
+        /**
+         * Get index of the triangle vertex in mesh (nodes vector).
+         * @param index index of vertex in the triangle corresponded to this element; equals to 0, 1 or 2
+         * @return index of the triangle vertex in mesh (nodes vector)
+         */
+        std::size_t getNodeIndex(std::size_t index) const noexcept {
+            assert(index < 3);
+            return triangleNodes[index];
+        }
+
+        /**
+         * Get coordinate of the triangle vertex.
+         * @param index index of vertex in the triangle corresponded to this element; equals to 0, 1 or 2
+         * @return coordinate of the triangle vertex
+         */
+        LocalCoords getNode(std::size_t index) const noexcept {
+            return mesh.nodes[getNodeIndex(index)];
+        }
+
+        /**
+         * Get centroid of the triangle corresponded to this element.
+         * @return centroid of the triangle corresponded to this element
+         */
+        LocalCoords getMidpoint() const {
+            return (getNode(0)+getNode(1)+getNode(2)) / 3.0;
+        }
+
+        double getArea() const noexcept {
+            // formula comes from http://www.mathguru.com/level2/application-of-coordinate-geometry-2007101600011139.aspx
+            const LocalCoords A = getNode(0);
+            const LocalCoords B = getNode(1);
+            const LocalCoords C = getNode(2);
+            return abs( (A.c0 - C.c0) * (B.c1 - A.c1)
+                      - (A.c0 - B.c0) * (C.c1 - A.c1) ) / 2.0;
         }
     };
 
+    struct Elements {
+        TriangularMesh2D& mesh;
+    };
 
+    /**
+     * Instance of this class allows for adding triangles to the mesh effectively.
+     */
+    struct Builder {
+        std::map<LocalCoords, std::size_t> indexOfNode; ///< map nodes to their indexes in mesh.nodes vector
+        TriangularMesh2D& mesh;
+
+        /**
+         * Construct builder which will add triangles to the given @p mesh.
+         * @param mesh triangles destination
+         */
+        explicit Builder(TriangularMesh2D& mesh);
+
+        /**
+         * Add a triangle to the mesh.
+         * @param p1, p2, p3 coordinates of vertices of the triangle to add
+         * @return <code>*this</code>
+         */
+        Builder& add(LocalCoords p1, LocalCoords p2, LocalCoords p3);
+
+    private:
+        std::size_t addNode(LocalCoords node);
+    };
+
+    // ------------------ Mesh and MeshD<2> interfaces: ------------------
+    LocalCoords at(std::size_t index) const override {
+        assert(index < nodes.size());
+        return nodes[index];
+    }
+
+    std::size_t size() const override {
+        return nodes.size();
+    }
+
+    bool empty() const override {
+        return nodes.empty();
+    }
+
+    // ---- Faster iterators used when exact type of mesh is known; they hide polimorphic iterators of parent class ----
+    iterator begin() { return nodes.begin(); }
+    iterator end() { return nodes.end(); }
+    const_iterator begin() const { return nodes.begin(); }
+    const_iterator end() const { return nodes.end(); }
 };
 
 }   // namespace plask
