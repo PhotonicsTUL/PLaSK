213853bf64c49ca4352ffd1bf0871e89663222d1
Piotr Beling <pbeling@emapa.pl>
2018-10-26 18:01:54 +0200

    generator_triangular.cpp: rewrite Triangle output to our triangle mesh, ref #241

--- plask/mesh/generator_triangular.cpp
+++ plask/mesh/generator_triangular.cpp
@@ -1,4 +1,5 @@
 #include "generator_triangular.h"
+#include "triangular2d.h"
 
 extern "C" {
 #include <triangle.h>
@@ -8,7 +9,20 @@ namespace plask {
 
 shared_ptr<MeshD<2>> TriangleGenerator::generate(const shared_ptr<GeometryObjectD<DIM> > &geometry) {
     triangulateio in, out;
+    // TODO prepare in and out
     triangulate(const_cast<char*>(getSwitches().c_str()), &in, &out, nullptr);
+
+    shared_ptr<TriangularMesh2D> result = make_shared<TriangularMesh2D>();
+    for (std::size_t i = 0; i < std::size_t(out.numberofpoints)*2; i += 2)
+        result->nodes.emplace_back(out.pointlist[i], out.pointlist[i+1]);
+    for (std::size_t i = 0; i < std::size_t(out.numberoftriangles)*3; i += 3)
+        result->elementsNodes.push_back({
+            std::size_t(out.trianglelist[i]),
+            std::size_t(out.trianglelist[i+1]),
+            std::size_t(out.trianglelist[i+2])
+        });
+        // it is also possible by triangle, to use 6 numbers per triangle, but we do not support such things at the moment
+    return result;
 }
 
 std::string TriangleGenerator::getSwitches() const {
