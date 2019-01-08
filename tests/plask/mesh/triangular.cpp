137aea94bb44b9750ed1503b02e850490b552fed
Piotr Beling <pbeling@emapa.pl>
2019-01-08 23:26:02 +0100

    work on triangular mesh tests

--- tests/plask/mesh/triangular.cpp
+++ tests/plask/mesh/triangular.cpp
@@ -15,9 +15,24 @@ BOOST_AUTO_TEST_CASE(triangular2d_simple) {
 }
 
 BOOST_AUTO_TEST_CASE(triangular2d_boundaries) {
-    auto node = [](std::size_t x_i, std::size_t y_i) { return x_i*10 + y_i; };
+    auto node = [](std::size_t x_i, std::size_t y_i) { return x_i*6 + y_i; };
     plask::TriangularMesh2D mesh;
     std::vector<std::size_t> all_boundary_indices;
+    /*
+     0  1  2  3  4  5
+
+     00-01-02-03-04-05  0
+     | \| \| \| \| \|
+     06-07-08-09-10-11  1
+     | \| \| \| \| \|
+     12-13-14 15-16-17  2
+     | \| \     \| \|
+     18-19-20-21-22-23  3
+     | \| \| \| \| \|
+     24-25-26-27-28-29  4
+     | \| \| \| \| \|
+     30-31-32-33-34-35  5
+     */
     for (std::size_t x_i = 0; x_i < 6; ++x_i)
         for (std::size_t y_i = 0; y_i < 6; ++y_i) {
             mesh.nodes.emplace_back(x_i, y_i);
@@ -37,6 +52,11 @@ BOOST_AUTO_TEST_CASE(triangular2d_boundaries) {
         plask::BoundaryNodeSet allBoundaries = mesh.getAllBoundary().get(mesh, plask::make_shared<plask::Geometry2DCartesian>());
         BOOST_CHECK_EQUAL(allBoundaries.size(), 5*4 /*outer*/ + 4 /*inner*/);
         BOOST_CHECK_EQUAL_COLLECTIONS(allBoundaries.begin(), allBoundaries.end(), all_boundary_indices.begin(), all_boundary_indices.end());
+    }
+    {
+        plask::BoundaryNodeSet allBoundariesIn = mesh.getAllBoundaryIn(plask::Box2D(0.5, 0.5, 4.5, 4.5)).get(mesh, plask::make_shared<plask::Geometry2DCartesian>());
+        BOOST_CHECK_EQUAL(allBoundariesIn.size(), 3*4 /*outer*/ + 4 /*inner*/);
+        //BOOST_CHECK_EQUAL_COLLECTIONS(allBoundariesIn.begin(), allBoundariesIn.end(), all_boundary_indices.begin(), all_boundary_indices.end());
     }
     {
         //plask::BoundaryNodeSet leftBoundary = mesh.
