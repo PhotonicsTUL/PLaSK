bcca741ad2fe2f2e471a710d8f997e2a54cd85f8
Piotr Beling <pbeling@emapa.pl>
2019-01-29 23:07:35 +0100

    some method of extruded triangular mesh, ref #544

--- plask/mesh/extruded_triangular3d.cpp
+++ plask/mesh/extruded_triangular3d.cpp
@@ -2,13 +2,51 @@
 
 namespace plask {
 
+inline Vec<3, double> from_longTran_vert(const Vec<2, double>& longTran, const double& vert) {
+    return vec(longTran.c0, longTran.c1, vert);
+}
+
+ExtrudedTriangularMesh3D::Element::Element(const ExtrudedTriangularMesh3D &mesh, std::size_t elementIndex)
+    : mesh(mesh)
+{
+    if (mesh.vertFastest) {
+        const std::size_t seg_size = mesh.vertAxis->size()-1;
+        longTranIndex = elementIndex / seg_size;
+        vertIndex = elementIndex % seg_size;
+    } else {
+        const std::size_t seg_size = mesh.longTranMesh.getElementsCount();
+        longTranIndex = elementIndex % seg_size;
+        vertIndex = elementIndex / seg_size;
+    }
+}
+
+Vec<3, double> ExtrudedTriangularMesh3D::Element::getMidpoint() const {
+    return from_longTran_vert(
+        longTranElement().getMidpoint(),
+        (mesh.vertAxis->at(vertIndex) + mesh.vertAxis->at(vertIndex+1)) * 0.5
+    );
+}
+
+double ExtrudedTriangularMesh3D::Element::getArea() const {
+    return longTranElement().getArea() *
+            (mesh.vertAxis->at(vertIndex+1) - mesh.vertAxis->at(vertIndex));
+}
+
+Box3D ExtrudedTriangularMesh3D::Element::getBoundingBox() const {
+    Box2D ltBox = longTranElement().getBoundingBox();
+    return Box3D(
+              from_longTran_vert(ltBox.lower, mesh.vertAxis->at(vertIndex)),
+              from_longTran_vert(ltBox.upper, mesh.vertAxis->at(vertIndex+1))
+           );
+}
+
 Vec<3, double> ExtrudedTriangularMesh3D::at(std::size_t index) const {
     if (vertFastest) {
-        Vec<2, double> longTran = longTranMesh[index / vertAxis->size()];
-        return vec(longTran.c0, longTran.c1, vertAxis->at(index % vertAxis->size()));
+        const std::size_t seg_size = vertAxis->size();
+        return from_longTran_vert(longTranMesh[index / seg_size], vertAxis->at(index % seg_size));
     } else {
-        Vec<2, double> longTran = longTranMesh[index % longTranMesh.size()];
-        return vec(longTran.c0, longTran.c1, vertAxis->at(index / longTranMesh.size()));
+        const std::size_t seg_size = longTranMesh.size();
+        return from_longTran_vert(longTranMesh[index % seg_size], vertAxis->at(index / seg_size));
     }
 }
 
@@ -20,11 +58,13 @@ bool ExtrudedTriangularMesh3D::empty() const {
     return longTranMesh.empty() || vertAxis->empty();
 }
 
-void ExtrudedTriangularMesh3D::writeXML(XMLElement &object) const
-{
+void ExtrudedTriangularMesh3D::writeXML(XMLElement &object) const {
     object.attr("type", "extruded_triangular2d");
     { auto a = object.addTag("vert"); vertAxis->writeXML(a); }
     { auto a = object.addTag("long_tran"); longTranMesh.writeXML(a); }
 }
+
+
+
 
 }   // namespace plask
--- plask/mesh/extruded_triangular3d.h
+++ plask/mesh/extruded_triangular3d.h
@@ -8,12 +8,53 @@ namespace plask {
 
 struct ExtrudedTriangularMesh3D: public MeshD<3> {
 
-    const shared_ptr<MeshAxis> vertAxis;
-
     TriangularMesh2D longTranMesh;
 
-    /// Iteration order, if true vert axis are changed the fastest, else it is changed the slowest.
+    const shared_ptr<MeshAxis> vertAxis;
+
+    /// Iteration order, if true vert axis is changed the fastest, else it is changed the slowest.
     bool vertFastest;
+
+    struct Element {
+        const ExtrudedTriangularMesh3D& mesh;
+        std::size_t longTranIndex, vertIndex;
+
+        Element(const ExtrudedTriangularMesh3D& mesh, std::size_t longTranIndex, std::size_t vertIndex)
+            : mesh(mesh), longTranIndex(longTranIndex), vertIndex(vertIndex) {}
+
+        Element(const ExtrudedTriangularMesh3D& mesh, std::size_t elementIndex);
+
+        /// @return index of this element
+        std::size_t getIndex() const { return mesh.elementIndex(longTranIndex, vertIndex); }
+
+        /// @return position of the middle of the element
+        inline Vec<3, double> getMidpoint() const;
+
+        /**
+         * Get area of this element.
+         * @return the area of the element
+         */
+        double getArea() const;
+
+        /**
+         * Check if point @p p is included in @c this element.
+         * @param p point to check
+         * @return @c true only if @p p is included in @c this
+         */
+        bool includes(Vec<3, double> p) const {
+            return mesh.vertAxis->at(vertIndex) <= p.vert() && p.vert() <= mesh.vertAxis->at(vertIndex+1)
+                    && mesh.longTranMesh.element(longTranIndex).includes(vec(p.lon(), p.tran()));
+        }
+
+        /**
+         * Calculate minimal box which contains this element.
+         * @return calculated box
+         */
+        Box3D getBoundingBox() const;
+
+    private:
+        TriangularMesh2D::Element longTranElement() const { return mesh.longTranMesh.element(longTranIndex); }
+    };
 
     Vec<3, double> at(std::size_t index) const override;
 
@@ -22,6 +63,39 @@ struct ExtrudedTriangularMesh3D: public MeshD<3> {
     bool empty() const override;
 
     void writeXML(XMLElement& object) const override;
+
+    /**
+     * Calculate index of this mesh using indexes of embeded meshes.
+     * @param longTranIndex index of longTranMesh
+     * @param vertIndex index of vertAxis
+     * @return the index of this mesh
+     */
+    std::size_t index(std::size_t longTranIndex, std::size_t vertIndex) const {
+        return vertFastest ?
+            longTranIndex * vertAxis->size() + vertIndex :
+            vertIndex * longTranMesh.size() + longTranIndex;
+    }
+
+    /**
+     * Calculate element index of this mesh using element indexes of embeded meshes.
+     * @param longTranIndex index of longTranMesh element
+     * @param vertIndex index of vertAxis element
+     * @return the element index of this mesh
+     */
+    std::size_t elementIndex(std::size_t longTranElementIndex, std::size_t vertElementIndex) const {
+        return vertFastest ?
+            longTranElementIndex * (vertAxis->size()-1) + vertElementIndex :
+            vertElementIndex * longTranMesh.getElementsCount() + longTranElementIndex;
+    }
+
+    /**
+     * Get number of elements in this mesh.
+     * @return number of elements
+     */
+    std::size_t getElementsCount() const {
+        const std::size_t vertSize = vertAxis->size();
+        return vertSize == 0 ? 0 : (vertSize-1) * longTranMesh.getElementsCount();
+    }
 };
 
 }   // namespace plask
