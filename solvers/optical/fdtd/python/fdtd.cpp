13d2ca0cb0af9b12bef8b7021549dba1e2427753
Maciej Dems <maciej.dems@p.lodz.pl>
2021-10-13 14:04:56 +0200

    Some Python renames

--- solvers/optical/fdtd/python/fdtd.cpp
+++ solvers/optical/fdtd/python/fdtd.cpp
@@ -24,22 +24,20 @@ static int HarminvResults__len__(const HarminvResults& self) {
 
 
 BOOST_PYTHON_MODULE(fdtd) {
-    {
-        CLASS(FDTDSolver, "FDTD2D", "Finite-differences time-domain solver")
-        METHOD(step, step, "Performs a single time-step.");
-        METHOD(compute, compute, "Performs a computation for a given simulation time.");
-        METHOD(outputHDF5, outputHDF5, "Outputs field data into a HDF5 file.");
-        METHOD(addFluxDFT, addFluxDFT, "Add DFT flux over a given region,");
-        METHOD(doHarminv, doHarminv, "",
-               (py::arg("component"), py::arg("point"), py::arg("wavelength"), py::arg("dl"), py::arg("time") = 200.,
-                py::arg("nbands") = 100));
-        METHOD(addFieldDFT, addFieldDFT, "");
-        RO_FIELD(courantFactor, "Courant factor");
-        RO_FIELD(elapsedTime, "Elapsed time in given in fs");
-        RO_FIELD(fieldMesh, "MEEP mesh equivalent");
-        PROVIDER(outLightH, "Provider for the H component");
-        PROVIDER(outLightE, "Provider for the E component");
-    }
+    CLASS(FDTDSolver, "FDTD2D", "Finite-differences time-domain solver")
+    METHOD(step, step, "Performs a single time-step.");
+    METHOD(compute, compute, "Performs a computation for a given simulation time.");
+    METHOD(output_hdf5, outputHDF5, "Outputs field data into a HDF5 file.");
+    METHOD(add_flux, addFluxDFT, "Add DFT flux over a given region,");
+    METHOD(do_harminv, doHarminv, "",
+            (py::arg("component"), py::arg("point"), py::arg("wavelength"), py::arg("dl"), py::arg("time") = 200.,
+            py::arg("nbands") = 100));
+    METHOD(add_field, addFieldDFT, "");
+    solver.def_readonly("courant_factor", &__Class__::courantFactor, "Courant factor");
+    solver.def_readonly("time", &__Class__::elapsedTime, "Elapsed time in given in fs");
+    solver.def_readonly("mesh", &__Class__::fieldMesh, "MEEP mesh equivalent");
+    PROVIDER(outLightH, "Provider for the H component");
+    PROVIDER(outLightE, "Provider for the E component");
 
     py::class_<HarminvValue, shared_ptr<HarminvValue>>("_HarminvValue", "Dokumentacja", py::no_init)
         .add_property("omega", &HarminvValue::omega, "Complex angular frequency")
