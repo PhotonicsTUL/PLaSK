b6f79f1b825429a4c4804dd4c7be936d7352bc8c
Maciej Dems <maciej.dems@p.lodz.pl>
2021-11-09 12:09:39 +0100

    Improved Nr parsing

--- plask/utils/lexical_cast.hpp
+++ plask/utils/lexical_cast.hpp
@@ -18,21 +18,28 @@ inline std::complex<double> lexical_cast(const std::string &arg)
     std::string src = boost::algorithm::trim_right_copy(arg);
 
     size_t n = src.length(), i;
+    bool negim = false;
 
     for (i = 0; i < n; ++i) {
         if (src[i] == 'e' || src[i] == 'E') {
             i++; continue;
         }
-        if (src[i] == '+' || src[i] == '-') break;
+        if (src[i] == '+')
+            break;
+        if (src[i] == '-') {
+            negim = true; break;
+        }
     }
 
     std::complex<double> result(0., 0.);
     try {
         result.real(boost::lexical_cast<double>(src.substr(0, i)));
         if (i < n) {
-            if (src[n-1] != 'j' && src[n-1] != 'J')
+            if (i > n-3 || (src[n-1] != 'j' && src[n-1] != 'J'))
                 boost::throw_exception(boost::bad_lexical_cast(typeid(std::string), typeid(std::complex<double>)));
-            result.imag(boost::lexical_cast<double>(src.substr(i, n-i-1)));
+            double im = boost::lexical_cast<double>(src.substr(i+1, n-i-2));
+            if (negim) im = -im;
+            result.imag(im);
         }
     } catch (...) {
          boost::throw_exception(boost::bad_lexical_cast(typeid(std::string), typeid(std::complex<double>)));
