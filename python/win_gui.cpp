2b8ee88dc3b204ec05ec3546edde5563b37356fc
Maciej Dems <maciej.dems@p.lodz.pl>
2022-01-19 23:59:01 +0100

    Better error messages for Windows [skipbuild]

--- python/win_gui.cpp
+++ python/win_gui.cpp
@@ -8,7 +8,16 @@ int WinMain(HINSTANCE hInst, HINSTANCE, LPSTR cmdline, int) {
     setupPythonPath();
     HMODULE plaskgui = LoadLibrary("plaskgui.dll");
     if (!plaskgui) {
-        std::cerr << "Could not import 'plaskgui.dll'\n";
+        LPSTR buffer;
+        auto err = GetLastError();
+        size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                                 NULL, err, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&buffer, 0, NULL);
+        std::string err_str(buffer, size);
+        LocalFree(buffer);
+        std::string message = "Could not start PLaSK GUI: " + err_str;
+        if (err == ERROR_MOD_NOT_FOUND)
+            message += "\nPlease verify if you have Python " PYTHON_VERSION_STR " installed from https://www.anaconda.com.";
+        MessageBox(NULL, message.c_str(), "PLaSK - Error", MB_OK | MB_ICONERROR);
         return 127;
     }
     MainProc plaskgui_main = (MainProc)GetProcAddress(plaskgui, "plaskgui_main");
--- python/win_main.cpp
+++ python/win_main.cpp
@@ -7,7 +7,15 @@ int wmain(int argc, const wchar_t *argv[]) {
     setupPythonPath();
     HMODULE plask = LoadLibrary("plaskexe.dll");
     if (!plask) {
-        std::cerr << "Could not import 'plaskexe.dll'\n";
+        LPSTR buffer;
+        auto err = GetLastError();
+        size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                                 NULL, err, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&buffer, 0, NULL);
+        std::string err_str(buffer, size);
+        LocalFree(buffer);
+        std::cerr << "Could not start 'plask': " << err_str;
+        if (err == ERROR_MOD_NOT_FOUND)
+            std::cerr << "-> Please verify if you have Python " PYTHON_VERSION_STR " installed from https://www.anaconda.com/.\n";
         return 127;
     }
     MainProc plask_main = (MainProc)GetProcAddress(plask, "plask_main");
